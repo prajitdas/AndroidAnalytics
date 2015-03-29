8fd21e70f364c88c72f980720e6e4f2c736b511c
Prajit Kumar Das <prajitdas@gmail.com>
2015-03-29 10:47:42 -0400

    analysis

---
+++ apkDecompile.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import os
+import sys
+import platform
+import shutil
+import subprocess
+from os import listdir
+from os.path import isfile, join
+
+def makeSurePathExists(path):
+	if os.path.exists(path):
+		return True
+	return False
+
+def deleteAndReCreateFolder(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+def runAnalysis(inpath,outPath):
+	'''
+		Run analysis
+	'''
+	os.chdir(inpath)
+	files = [ f for f in listdir(inpath) if isfile(join(inpath,f)) ]
+	for file in files:
+		subprocess.call(["apktool", "d", "-f", inpath+file, "-o", outPath], shell=True)
+
+def main(argv):
+	if len(sys.argv) != 1:
+		sys.stderr.write('Usage: python analysis.py\n')
+		sys.exit(1)
+		
+	'''
+		Detect operating system and takes actions accordingly
+	'''
+	osInfo = platform.system()
+	if osInfo == 'Windows':
+		appsFolder = os.path.dirname(os.path.realpath(__file__))+"\\apps\\"
+		outFolder = os.path.dirname(os.path.realpath(__file__))+"\\apps\\"
+	elif osInfo == 'Linux':
+		appsFolder = os.path.dirname(os.path.realpath(__file__))+"/data/"
+		outFolder = os.path.dirname(os.path.realpath(__file__))+"\\data\\"
+	else:
+		print 'The current os not supported at the moment.'
+
+	if makeSurePathExists(appsFolder) && makeSurePathExists(outFolder):
+		runAnalysis(appsFolder,outFolder)
+	else:
+		print 'The data folder doesn\'t exist. Create one and download apks to it and then run this script again.'
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv))
---
+++ installApkTool.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import os
+import sys
+import platform
+import subprocess
+import shutil
+
+def makeSurePathExists(wrapperSrc,jarSrc,des):
+	if os.path.exists(wrapperSrc) and os.path.exists(jarSrc) and os.path.exists(des):
+		return True
+	return False
+
+def installApkTool(wrapperSrc,jarSrc,des):
+	'''
+		Install ApkTool
+	'''
+	shutil.copy(wrapperSrc, des)
+	shutil.copy(jarSrc, des)
+
+def main(argv):
+	if len(sys.argv) != 1:
+		sys.stderr.write('Usage: python installApkTool.py\n')
+		sys.exit(1)
+		
+	'''
+		Detect operating system and takes installation actions accordingly
+	'''
+	osInfo = platform.system()
+	dirpath = os.path.dirname(os.path.realpath(__file__))
+	if osInfo == 'Windows':
+		src = dirpath+"\\apktool"
+		wrapperSrc = src+"\\apktool.bat"
+		jarSrc = src+"\\apktool.jar"
+		des = os.environ['WINDIR']
+	elif osInfo == 'Linux':
+		src = os.path.abspath(dirpath+"/apktool")
+		wrapperSrc = src+"/apktool"
+		jarSrc = src+"/apktool.jar"
+		des = "/usr/local/bin"
+	else:
+		print 'The current os not supported at the moment.'
+
+	if makeSurePathExists(wrapperSrc,jarSrc,des):
+		installApkTool(wrapperSrc,jarSrc,des)
+	else:
+		print 'The ApkTool folder does not exist. Please create it and download the ApkTool files there and then execute this script again.'
+
+	if osInfo == 'Linux':
+		subprocess.call(['chmod', '0755', wrapperSrc])
+		subprocess.call(['chmod', '0755', jarSrc])
+	
+if __name__ == "__main__":
+	sys.exit(main(sys.argv))

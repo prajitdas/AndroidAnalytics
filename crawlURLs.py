6fedfc82494ed4e9c80760fc2f0d2b5801579d38
Prajit Kumar Das <prajitdas@gmail.com>
2015-03-29 03:26:39 -0400

    download apk first commit

--- crawlURLs.py
+++ crawlURLs.py
@@ -1,9 +1,10 @@
 from dateutil.relativedelta import relativedelta
 from bs4 import BeautifulSoup
-import urllib, re, urllib, simplejson, sys, datetime, MySQLdb, _mysql_exceptions
+import urllib, re, simplejson, sys, datetime, MySQLdb, _mysql_exceptions
 
 count = 0
 
+# Fire an DML SQL statement and commit data
 def dbManipulateData(dbHandle, sqlStatement):
 	cursor = dbHandle.cursor()
 	try:
@@ -15,7 +16,8 @@ def dbManipulateData(dbHandle, sqlStatement):
 		print "Unexpected error:", sys.exc_info()[0]
 		raise
 
-def extractDataAndStore(dbHandle, urlExtract):
+# Hit a URL, extract URLs and Store new URLs back
+def extractMoreURLsAndStore(dbHandle, urlExtract):
 	page = urllib.urlopen(urlExtract).read()
 	soup = BeautifulSoup(''.join(page))
 	data = soup.findAll(attrs={'class': 'card-click-target'})
@@ -27,12 +29,53 @@ def extractDataAndStore(dbHandle, urlExtract):
 		sqlStatement = "INSERT INTO appurls(app_pkg_name, app_url) VALUES('"+packageName[1]+"', '"+url+"');"
 		dbManipulateData(dbHandle, sqlStatement)
 
+# Update "urls_extracted" column to mark urls have been extracted
+def updateURLsExtracted(dbHandle, id):
+	cursor = dbHandle.cursor()
+	sqlStatement = "UPDATE appurls SET urls_extracted=1 WHERE id="+str(id)+";"
+	dbManipulateData(dbHandle, sqlStatement)
+
+# Get URLs for extracting more URLs
+def getURLsForExtractingMoreURLs(dbHandle):
+	cursor = dbHandle.cursor()
+	sqlStatement = "SELECT id, app_url FROM appurls WHERE urls_extracted = 0;"
+	try:
+		cursor.execute(sqlStatement)
+		queryOutput = cursor.fetchall()
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+		raise
+	for row in queryOutput:
+		updateURLsExtracted(dbHandle,row[0])
+		extractMoreURLsAndStore(dbHandle,row[1])
+
+# Initial data collection using basic alphabetical search for apps
+def oneTimeCreateListOfAppsFromAlphabeticalSearch(dbHandle):
+	alpahbeticalSearchStrings=["https://play.google.com/store/search?q=a&c=apps","https://play.google.com/store/search?q=b&c=apps","https://play.google.com/store/search?q=c&c=apps","https://play.google.com/store/search?q=d&c=apps","https://play.google.com/store/search?q=e&c=apps","https://play.google.com/store/search?q=f&c=apps","https://play.google.com/store/search?q=g&c=apps","https://play.google.com/store/search?q=h&c=apps","https://play.google.com/store/search?q=i&c=apps","https://play.google.com/store/search?q=j&c=apps","https://play.google.com/store/search?q=k&c=apps","https://play.google.com/store/search?q=l&c=apps","https://play.google.com/store/search?q=m&c=apps","https://play.google.com/store/search?q=n&c=apps","https://play.google.com/store/search?q=o&c=apps","https://play.google.com/store/search?q=p&c=apps","https://play.google.com/store/search?q=q&c=apps","https://play.google.com/store/search?q=r&c=apps","https://play.google.com/store/search?q=s&c=apps","https://play.google.com/store/search?q=t&c=apps","https://play.google.com/store/search?q=u&c=apps","https://play.google.com/store/search?q=v&c=apps","https://play.google.com/store/search?q=w&c=apps","https://play.google.com/store/search?q=x&c=apps","https://play.google.com/store/search?q=y&c=apps","https://play.google.com/store/search?q=z&c=apps"]
+	for searchString in alpahbeticalSearchStrings:
+		extractURLAndStore(dbHandle, searchString)
+
+# Extract app data and store in DB
+def extractAppDataAndStore(dbHandle, urlExtract):
+	page = urllib.urlopen(urlExtract).read()
+	soup = BeautifulSoup(''.join(page))
+	data = soup.findAll(attrs={'class': 'card-click-target'})
+
+	for chunk in data:
+		count = count + 1
+		url = "https://play.google.com"+chunk['href']
+		packageName = url.split("=")
+		sqlStatement = "INSERT INTO appurls(app_pkg_name, app_url) VALUES('"+packageName[1]+"', '"+url+"');"
+		dbManipulateData(dbHandle, sqlStatement)
+
+# Update "parsed" column to mark app data has been parsed
 def updateParsed(dbHandle, id):
 	cursor = dbHandle.cursor()
 	sqlStatement = "UPDATE appurls SET parsed=1 WHERE id="+str(id)+";"
 	dbManipulateData(dbHandle, sqlStatement)
 
-def appUrlGeneration(dbHandle):
+# Get URLs for app data parsing
+def getURLsForParsingAppData(dbHandle):
 	cursor = dbHandle.cursor()
 	sqlStatement = "SELECT id, app_url FROM appurls WHERE parsed = 0;"
 	try:
@@ -43,20 +86,16 @@ def appUrlGeneration(dbHandle):
 		raise
 	for row in queryOutput:
 		updateParsed(dbHandle,row[0])
-		extractDataAndStore(dbHandle,row[1])
+		extractAppDataAndStore(dbHandle,row[1])
 
-def oneTimeCreateListOfAppsFromAlphabeticalSearch(dbHandle):
-	alpahbeticalSearchStrings=["https://play.google.com/store/search?q=a&c=apps","https://play.google.com/store/search?q=b&c=apps","https://play.google.com/store/search?q=c&c=apps","https://play.google.com/store/search?q=d&c=apps","https://play.google.com/store/search?q=e&c=apps","https://play.google.com/store/search?q=f&c=apps","https://play.google.com/store/search?q=g&c=apps","https://play.google.com/store/search?q=h&c=apps","https://play.google.com/store/search?q=i&c=apps","https://play.google.com/store/search?q=j&c=apps","https://play.google.com/store/search?q=k&c=apps","https://play.google.com/store/search?q=l&c=apps","https://play.google.com/store/search?q=m&c=apps","https://play.google.com/store/search?q=n&c=apps","https://play.google.com/store/search?q=o&c=apps","https://play.google.com/store/search?q=p&c=apps","https://play.google.com/store/search?q=q&c=apps","https://play.google.com/store/search?q=r&c=apps","https://play.google.com/store/search?q=s&c=apps","https://play.google.com/store/search?q=t&c=apps","https://play.google.com/store/search?q=u&c=apps","https://play.google.com/store/search?q=v&c=apps","https://play.google.com/store/search?q=w&c=apps","https://play.google.com/store/search?q=x&c=apps","https://play.google.com/store/search?q=y&c=apps","https://play.google.com/store/search?q=z&c=apps"]
-	for searchString in alpahbeticalSearchStrings:
-		extractDataAndStore(dbHandle, searchString)
-
+# Database Conenction Handler
 def dbConnectionCheck():
 	dbHandle = MySQLdb.connect('trishuli.cs.umbc.edu', 'googleplaystore', 'prajitkumardas', 'googleplaystore');
 	return dbHandle
 
 def main(argv):
 	if len(sys.argv) != 2:
-		sys.stderr.write('Usage: python %s [i|c]\n')
+		sys.stderr.write('Usage: python crawlURLs [i|e|p]\n')
 		sys.exit(1)
 
 	dbHandle = dbConnectionCheck() # DB Open
@@ -64,12 +103,15 @@ def main(argv):
 	startTime = datetime.datetime.now()
 	if sys.argv[1] == "i":
 		oneTimeCreateListOfAppsFromAlphabeticalSearch(dbHandle) # First level of search for app urls
-	elif sys.argv[1] == "c":
-		appUrlGeneration(dbHandle) # Second level of search for app urls
+	elif sys.argv[1] == "e":
+		getURLsForExtractingMoreURLs(dbHandle) # Second level of search for app urls
+	elif sys.argv[1] == "p":
+		getURLsForParsingAppData(dbHandle) # Extract app data
 	else:
-		sys.stderr.write('Usage: python %s [i|c]\n')
+		sys.stderr.write('Usage: python crawlURLs [i|e|p]\n')
 	endTime = datetime.datetime.now()
 	print "Execution time was: "(endTime-startTime)
+
 	dbHandle.close() #DB Close
 
 if __name__ == "__main__":
--- downloadAPKs.py
+++ downloadAPKs.py
@@ -1,9 +1,16 @@
+#!/usr/bin/python
+
 from dateutil.relativedelta import relativedelta
 from bs4 import BeautifulSoup
-import urllib, re, urllib, simplejson, sys, datetime, MySQLdb, _mysql_exceptions
+import urllib
+import re
+import simplejson
+import sys
+import datetime
+import MySQLdb
+import _mysql_exceptions
 
-count = 0
-
+# Fire an DML SQL statement and commit data
 def dbManipulateData(dbHandle, sqlStatement):
 	cursor = dbHandle.cursor()
 	try:
@@ -15,26 +22,23 @@ def dbManipulateData(dbHandle, sqlStatement):
 		print "Unexpected error:", sys.exc_info()[0]
 		raise
 
-def extractDataAndStore(dbHandle, urlExtract):
-	page = urllib.urlopen(urlExtract).read()
-	soup = BeautifulSoup(''.join(page))
-	data = soup.findAll(attrs={'class': 'card-click-target'})
+# Download APK
+def downloadAPK(appPackageName):
+	URLPrefix = "http://dl3.apk-dl.com/store/download/details?id="
+	APKURL = URLPrefix+appPackageName
+	appDownloadedFileLocation = "apps/"+appPackageName
+	urllib.urlretrieve(APKURL, filename=appDownloadedFileLocation)
 
-	for chunk in data:
-		count = count + 1
-		url = "https://play.google.com"+chunk['href']
-		packageName = url.split("=")
-		sqlStatement = "INSERT INTO appurls(app_pkg_name, app_url) VALUES('"+packageName[1]+"', '"+url+"');"
-		dbManipulateData(dbHandle, sqlStatement)
-
-def updateParsed(dbHandle, id):
+# Update "downloaded" column to mark app has been downloaded
+def updateDownloaded(dbHandle, id):
 	cursor = dbHandle.cursor()
-	sqlStatement = "UPDATE appurls SET parsed=1 WHERE id="+str(id)+";"
+	sqlStatement = "UPDATE appurls SET downloaded=1 WHERE id="+str(id)+";"
 	dbManipulateData(dbHandle, sqlStatement)
 
-def appUrlGeneration(dbHandle):
+# Get URLs for app downloading
+def getAppURL(dbHandle):
 	cursor = dbHandle.cursor()
-	sqlStatement = "SELECT id, app_url FROM appurls WHERE parsed = 0;"
+	sqlStatement = "SELECT id, app_pkg_name FROM appurls WHERE downloaded = 0 AND id = 1;"
 	try:
 		cursor.execute(sqlStatement)
 		queryOutput = cursor.fetchall()
@@ -42,34 +46,26 @@ def appUrlGeneration(dbHandle):
 		print "Unexpected error:", sys.exc_info()[0]
 		raise
 	for row in queryOutput:
-		updateParsed(dbHandle,row[0])
-		extractDataAndStore(dbHandle,row[1])
+		updateDownloaded(dbHandle,row[0])
+		downloadAPK(row[1])
 
-def oneTimeCreateListOfAppsFromAlphabeticalSearch(dbHandle):
-	alpahbeticalSearchStrings=["https://play.google.com/store/search?q=a&c=apps","https://play.google.com/store/search?q=b&c=apps","https://play.google.com/store/search?q=c&c=apps","https://play.google.com/store/search?q=d&c=apps","https://play.google.com/store/search?q=e&c=apps","https://play.google.com/store/search?q=f&c=apps","https://play.google.com/store/search?q=g&c=apps","https://play.google.com/store/search?q=h&c=apps","https://play.google.com/store/search?q=i&c=apps","https://play.google.com/store/search?q=j&c=apps","https://play.google.com/store/search?q=k&c=apps","https://play.google.com/store/search?q=l&c=apps","https://play.google.com/store/search?q=m&c=apps","https://play.google.com/store/search?q=n&c=apps","https://play.google.com/store/search?q=o&c=apps","https://play.google.com/store/search?q=p&c=apps","https://play.google.com/store/search?q=q&c=apps","https://play.google.com/store/search?q=r&c=apps","https://play.google.com/store/search?q=s&c=apps","https://play.google.com/store/search?q=t&c=apps","https://play.google.com/store/search?q=u&c=apps","https://play.google.com/store/search?q=v&c=apps","https://play.google.com/store/search?q=w&c=apps","https://play.google.com/store/search?q=x&c=apps","https://play.google.com/store/search?q=y&c=apps","https://play.google.com/store/search?q=z&c=apps"]
-	for searchString in alpahbeticalSearchStrings:
-		extractDataAndStore(dbHandle, searchString)
-
+# Database Conenction Handler
 def dbConnectionCheck():
 	dbHandle = MySQLdb.connect('trishuli.cs.umbc.edu', 'googleplaystore', 'prajitkumardas', 'googleplaystore');
 	return dbHandle
 
 def main(argv):
 	if len(sys.argv) != 2:
-		sys.stderr.write('Usage: python %s [i|c]\n')
+		sys.stderr.write('Usage: python downloadAPKs [i|e|p]\n')
 		sys.exit(1)
 
 	dbHandle = dbConnectionCheck() # DB Open
 
+	getAppURL(dbHandle)
 	startTime = datetime.datetime.now()
-	if sys.argv[1] == "i":
-		oneTimeCreateListOfAppsFromAlphabeticalSearch(dbHandle) # First level of search for app urls
-	elif sys.argv[1] == "c":
-		appUrlGeneration(dbHandle) # Second level of search for app urls
-	else:
-		sys.stderr.write('Usage: python %s [i|c]\n')
 	endTime = datetime.datetime.now()
 	print "Execution time was: "(endTime-startTime)
+
 	dbHandle.close() #DB Close
 
 if __name__ == "__main__":

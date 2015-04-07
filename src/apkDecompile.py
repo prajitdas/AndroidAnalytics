afe835bafe15d82eaff630919a171579d1019965
Prajit Kumar Das <prajitdas@gmail.com>
2015-04-07 18:06:20 -0400

    permissions id protection level not available bug

--- src/apkDecompile.py
+++ src/apkDecompile.py
@@ -97,7 +97,27 @@ def extractCustomPermissions(soup):
 	for message in soup.findAll('permission'):
 		permissionsAttributes = dict(message.attrs)
 		permissionName = permissionsAttributes['android:name']
-		permissionProtectionLevel = permissionsAttributes['android:protectionlevel']
+		if 'android:protectionLevel' in permissionsAttributes:
+			permissionProtectionLevel = permissionsAttributes['android:protectionLevel']
+		else:
+			'''
+				android:protectionLevel: Characterizes the potential risk implied in the permission and indicates the procedure the system should follow when determining 
+				whether or not to grant the permission to an application requesting it. The value can be set to one of the following strings:
+				"normal": 				The default value. A lower-risk permission that gives requesting applications access to isolated application-level features, with minimal risk 
+										to other applications, the system, or the user. The system automatically grants this type of permission to a requesting application at installation, 
+										without asking for the user's explicit approval (though the user always has the option to review these permissions before installing).
+				"dangerous":			A higher-risk permission that would give a requesting application access to private user data or control over the device that can negatively impact the user. 
+										Because this type of permission introduces potential risk, the system may not automatically grant it to the requesting application. 
+										For example, any dangerous permissions requested by an application may be displayed to the user and require confirmation before proceeding, 
+										or some other approach may be taken to avoid the user automatically allowing the use of such facilities.
+				"signature":			A permission that the system grants only if the requesting application is signed with the same certificate as the application that declared the 
+										permission. If the certificates match, the system automatically grants the permission without notifying the user or asking for the user's explicit approval.
+				"signatureOrSystem":	A permission that the system grants only to applications that are in the Android system image or that are signed with the same certificate as the 
+										application that declared the permission. Please avoid using this option, as the signature protection level should be sufficient for most needs and works 
+										regardless of exactly where applications are installed. The "signatureOrSystem" permission is used for certain special situations where multiple vendors 
+										have applications built into a system image and need to share specific features explicitly because they are being built together.
+			'''
+			permissionProtectionLevel = 'normal'
 		if not verifyIfPermissionIsInTable(permissionName):
 			sqlStatement = "INSERT INTO `permissions`(`name`,`protection_level`) VALUES ('"+permissionName+"','"+permissionProtectionLevel+"');"
 			dbHandle = databaseHandler.dbConnectionCheck()

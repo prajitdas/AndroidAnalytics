36aac33eb94f15387ad2f7731731a41cae71ccb2
Prajit Kumar Das <prajitdas@gmail.com>
2015-08-03 12:00:41 -0400

    reading the android manifest file for default permissions and will generate graph for those permissions

--- code/src/org/ebiquity/data/analysis/generatePermissionsRequestedByAppFrequencyHistogram.py
+++ code/src/org/ebiquity/data/analysis/generatePermissionsRequestedByAppFrequencyHistogram.py
@@ -6,9 +6,15 @@ Created on August 3, 2015
 Usage: python generatePermissionsRequestedByAppFrequencyHistogram.py username api_key
 '''
 
+from bs4 import BeautifulSoup
+import urllib2
 import sys
-import time
 import databaseHandler
+import time
+import datetime
+import traceback
+import httplib
+import xmltodict
 import plotly.tools as tls
 # Learn about API authentication here: https://plot.ly/python/getting-started
 # Find your api_key here: https://plot.ly/settings/api
@@ -18,15 +24,15 @@ from plotly.graph_objs import *
 # This is a plot for Permissions count vs Frequency of apps requesting that many permissions
 def generatePlot(username, api_key, permName, appCount):
     tls.set_credentials_file(username, api_key)
-    trace = Bar(
+    tracePerm = Bar(
         x=permName,
         y=appCount,
         name='App frequency count',
         marker=Marker(
-            color='rgb(55, 83, 109)'
+            color='rgb(55, 83, 100)'
         )
     )
-    data = Data([trace])
+    data = Data([tracePerm])
     layout = Layout(
         title='Permission vs App frequency count',
         xaxis=XAxis(
@@ -64,9 +70,36 @@ def generatePlot(username, api_key, permName, appCount):
     fig = Figure(data=data, layout=layout)
     plot_url = py.plot(fig, filename='style-bar')
     print "Check out the URL: "+plot_url+" for your plot"
+
+# Hit a URL, extract permissions from the url
+def crawlUrl(dbHandle, urlExtract):
+    headers = { 'User-Agent' : 'Mozilla/5.0' }
+    req = urllib2.Request(urlExtract, None, headers)
+    try: 
+        page = urllib2.urlopen(req).read()
+        xmlParsedDict = xmltodict.parse(page)
+        print xmlParsedDict
+        
+    except urllib2.HTTPError, e:
+        print 'HTTPError = ', str(e.code)
+        #This is risky!!!!
+        # June 19, 2015: Yes, this was risky commenting out this piece of code - Prajit
+        # Don't be ridiculous, once you have collected some data you should not be deleting that data, right?
+        # Have to monitor this properly
+        #sqlStatement = "DELETE FROM `appurls` WHERE `app_url` = '"+urlExtract+"';"
+        #databaseHandler.dbManipulateData(dbHandle, sqlStatement)
+    except urllib2.URLError, e:
+        print 'URLError = ' + str(e.reason)
+    except httplib.HTTPException, e:
+        print 'HTTPException'
+    except Exception:
+        print 'generic exception: ' + traceback.format_exc()
  
 def doTask(username, api_key):
     dbHandle = databaseHandler.dbConnectionCheck() #DB Open
+
+    crawlUrl(dbHandle, "https://raw.githubusercontent.com/android/platform_frameworks_base/master/core/res/AndroidManifest.xml")    
+    sys.exit(1)
 
     cursor = dbHandle.cursor()
     sqlStatement = "SELECT * FROM `perm_app_count_view`;"

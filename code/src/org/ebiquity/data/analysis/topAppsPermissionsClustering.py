12a4266474d0812ee0ca8e245dac2de03b6976ac
Prajit Kumar Das <prajitdas@gmail.com>
2015-08-04 18:33:02 -0400

    silhouette_avg added to code

--- code/src/org/ebiquity/data/analysis/topAppsPermissionsClustering.py
+++ code/src/org/ebiquity/data/analysis/topAppsPermissionsClustering.py
@@ -152,14 +152,29 @@ def doTask(username, api_key, predictedClustersFile, appMatrixFile):
     # We want to verify if the number of clusters are "strong with this one" (or not)
     #Run clustering with a varying number of clusters
     for numberOfClusters in range(startingNumberOfClusters,endingNumberOfClusters):
-        loopListEvaluatedCluster = []
-        KMeansObject = skcl.KMeans(numberOfClusters)
+        # Start of code from: http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
+        # Create a subplot with 1 row and 2 columns
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        fig.set_size_inches(18, 7)
+    
+        # The 1st subplot is the silhouette plot
+        # The silhouette coefficient can range from -1, 1 but in this example all
+        # lie within [-0.1, 1]
+        ax1.set_xlim([-0.1, 1])
+        # The (n_clusters+1)*10 is for inserting blank space between silhouette
+        # plots of individual clusters, to demarcate them clearly.
+        ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+        # End of code from: http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
+
         print "Running clustering algorithm with", numberOfClusters, "clusters"
-        clusters = KMeansObject.fit_predict(appMatrix)
+        loopListEvaluatedCluster = []
+        # Initialize the clusterer with numberOfClusters value 
+        KMeansObject = skcl.KMeans(numberOfClusters)
+        clusterLabelsAssigned = KMeansObject.fit_predict(appMatrix)
         counter = 0
         predictedClusters = {}
         for appName in appVector:
-            predictedClusters[appName] = clusters[counter]
+            predictedClusters[appName] = clusterLabelsAssigned[counter]
             counter = counter + 1
             
         loopListEvaluatedCluster.append(predictedClusters)
@@ -176,7 +191,6 @@ def doTask(username, api_key, predictedClustersFile, appMatrixFile):
         #Clustering task is complete. Now evaluate
 #         evaluationOutput = clEval.evaluateCluster(json.loads(open(predictedClustersFile, 'r').read().decode('utf8')))
         clusterEvaluationResults = clEval.evaluateCluster(predictedClusters)
-
 #         print clusterEvaluationResults["adjusted_rand_score"]
 #         print clusterEvaluationResults["adjusted_mutual_info_score"]
 #         print clusterEvaluationResults["homogeneity_score"]
@@ -184,6 +198,81 @@ def doTask(username, api_key, predictedClustersFile, appMatrixFile):
 #         print clusterEvaluationResults["v_measure_score"]
 
         loopListEvaluatedCluster.append(clusterEvaluationResults)
+
+        # Start of code from: http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
+        # The silhouette_score gives the average value for all the samples.
+        # This gives a perspective into the density and separation of the formed
+        # clusters
+        silhouette_avg = silhouette_score(appMatrix, clusterLabelsAssigned)
+        clusterSilhouetteAverage = {}
+        clusterSilhouetteAverage["silhouette_avg"] = silhouette_avg
+        print("For number of clusters =", numberOfClusters,
+              "The average silhouette_score is :", silhouette_avg)
+    
+        # Compute the silhouette scores for each sample
+        sample_silhouette_values = silhouette_samples(appMatrix, clusterLabelsAssigned)
+        print sample_silhouette_values
+    
+        y_lower = 10
+        for i in range(n_clusters):
+            # Aggregate the silhouette scores for samples belonging to
+            # cluster i, and sort them
+            ith_cluster_silhouette_values = \
+                sample_silhouette_values[cluster_labels == i]
+    
+            ith_cluster_silhouette_values.sort()
+    
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+    
+            color = cm.spectral(float(i) / n_clusters)
+            ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                              0, ith_cluster_silhouette_values,
+                              facecolor=color, edgecolor=color, alpha=0.7)
+    
+            # Label the silhouette plots with their cluster numbers at the middle
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+    
+            # Compute the new y_lower for next plot
+            y_lower = y_upper + 10  # 10 for the 0 samples
+    
+        ax1.set_title("The silhouette plot for the various clusters.")
+        ax1.set_xlabel("The silhouette coefficient values")
+        ax1.set_ylabel("Cluster label")
+    
+        # The vertical line for average silhoutte score of all the values
+        ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+    
+        ax1.set_yticks([])  # Clear the yaxis labels / ticks
+        ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+    
+        # 2nd Plot showing the actual clusters formed
+        colors = cm.spectral(cluster_labels.astype(float) / n_clusters)
+        ax2.scatter(X[:, 0], X[:, 1], marker='.', s=30, lw=0, alpha=0.7,
+                    c=colors)
+    
+        # Labeling the clusters
+        centers = clusterer.cluster_centers_
+        # Draw white circles at cluster centers
+        ax2.scatter(centers[:, 0], centers[:, 1],
+                    marker='o', c="white", alpha=1, s=200)
+    
+        for i, c in enumerate(centers):
+            ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1, s=50)
+    
+        ax2.set_title("The visualization of the clustered data.")
+        ax2.set_xlabel("Feature space for the 1st feature")
+        ax2.set_ylabel("Feature space for the 2nd feature")
+    
+        plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+                      "with n_clusters = %d" % n_clusters),
+                     fontsize=14, fontweight='bold')
+    
+        plt.show()
+        # End of code from: http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html
+        
+        # Insert the silhouette_avg for the cluster into the Json for further evaluation
+        loopListEvaluatedCluster.append(clusterSilhouetteAverage)
         
         stringLoopCounter = 'Loop'+str(loopCounter)
         evaluatedClusterResultsDict[stringLoopCounter] = loopListEvaluatedCluster

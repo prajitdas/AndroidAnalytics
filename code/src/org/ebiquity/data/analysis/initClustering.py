390dc91462b76659eedbb50211227c0ddac9140c
Prajit Kumar Das <prajitdas@gmail.com>
2015-08-18 15:08:58 -0400

    combining common actions into one

---
+++ code/src/org/ebiquity/data/analysis/initClustering.py
@@ -0,0 +1,165 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on August 14, 2015
+@author: Prajit
+Usage: python initClustering.py username api_key
+'''
+import numpy as np
+import sys
+import time
+import databaseHandler
+import io
+import runClustering as runCl
+import selectApps
+import selectPermissions
+import cPickle
+
+#generate the permission matrix for category list apps
+def generateAppMatrixCatApps(dbHandle):
+    cursor = dbHandle.cursor()
+    
+    #select the apps to be processed
+    appDict = selectApps.getCategoryApps(dbHandle,appCategoryList)
+    appMatrix = []
+    appVector = []
+    for appPackageName, appId in appDict.iteritems():
+        #extract the permissions vector for each app
+        permVector = selectPermissions.extractAppPermisionVector(dbHandle,appId)
+        appVector.append(appPackageName)
+        appMatrix.append(permVector)
+    
+    #Return app vector appMatrix will be read from File
+    return appVector, appMatrix
+
+#Initiate the clustering process for category list apps
+def initClusteringCatApps(username, api_key, appCategoryList, predictedClustersFile, appMatrixFile):
+    dbHandle = databaseHandler.dbConnectionCheck() #DB Open
+
+    #Generate app matrix file once
+    appVector, appMatrix = selectPermissions.generateAppMatrix(dbHandle,appCategoryList)
+
+    #Once the whole matrix is created then dump to a file
+    #Write the app permissions matrix to a file            
+    cPickle.dump(appMatrix, open(appMatrixFile, 'wb'))
+    appMatrix = cPickle.load(open(appMatrixFile, 'rb'))
+    newAppMatrix = np.array(appMatrix)
+
+    runCl.runClustering(username, api_key, appCategoryList, predictedClustersFile, newAppMatrix, appVector)
+    dbHandle.close() #DB Close
+
+#generate the permission matrix for all apps
+def generateAppMatrixAllApps(dbHandle):
+    cursor = dbHandle.cursor()
+    
+    #select the apps to be processed
+    appDict = selectApps.getTopApps(dbHandle)
+    appMatrix = []
+    appVector = []
+    for appPackageName, appId in appDict.iteritems():
+        #extract the permissions vector for each app
+        permVector = selectPermissions.extractAppPermisionVector(dbHandle,appId)
+        appVector.append(appPackageName)
+        appMatrix.append(permVector)
+    
+    #Return app vector appMatrix will be read from File
+    return appVector, appMatrix
+
+#Initiate the clustering process for all apps
+def initClusteringAllApps(username, api_key, predictedClustersFile, appMatrixFile):
+    dbHandle = databaseHandler.dbConnectionCheck() #DB Open
+
+    #Generate app matrix file once
+    appVector, appMatrix = selectPermissions.generateAppMatrix(dbHandle)
+
+    #Once the whole matrix is created then dump to a file
+    #Write the app permissions matrix to a file            
+    cPickle.dump(appMatrix, open(appMatrixFile, 'wb'))
+    appMatrix = cPickle.load(open(appMatrixFile, 'rb'))
+    newAppMatrix = np.array(appMatrix)
+
+    runCl.runClustering(username, api_key, ['all'], predictedClustersFile, newAppMatrix, appVector)
+    dbHandle.close() #DB Close
+
+#generate the permission matrix for top apps
+def generateAppMatrixTopApps(dbHandle):
+    cursor = dbHandle.cursor()
+    
+    #select the apps to be processed
+    appDict = selectApps.getTopApps(dbHandle)
+    appMatrix = []
+    appVector = []
+    for appPackageName, appId in appDict.iteritems():
+        #extract the permissions vector for each app
+        permVector = selectPermissions.extractAppPermisionVector(dbHandle,appId)
+        appVector.append(appPackageName)
+        appMatrix.append(permVector)
+    
+    #Return app vector appMatrix will be read from File
+    return appVector, appMatrix
+
+#Initiate the clustering process for top apps
+def initClusteringTopApps(username, api_key, predictedClustersFile, appMatrixFile):
+    dbHandle = databaseHandler.dbConnectionCheck() #DB Open
+
+    #Generate app matrix file once
+    appVector, appMatrix = generateAppMatrixTopApps(dbHandle)
+
+    #Once the whole matrix is created then dump to a file
+    #Write the app permissions matrix to a file            
+    cPickle.dump(appMatrix, open(appMatrixFile, 'wb'))
+    appMatrix = cPickle.load(open(appMatrixFile, 'rb'))
+    newAppMatrix = np.array(appMatrix)
+
+    runCl.runClustering(username, api_key, ['top'], predictedClustersFile, newAppMatrix, appVector)
+    dbHandle.close() #DB Close
+
+def preProcess(selectionType):
+    if selectionType == 'top':
+        appCategoryList = ['top']
+    elif selectionType == 'med':
+        appCategoryList = ['https://play.google.com/store/apps/category/MEDICAL']
+    elif selectionType == 'hea':
+        appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS']
+    elif selectionType == 'hmd':
+        appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/MEDICAL']
+    elif selectionType == 'all':
+        appCategoryList = ['all']
+    '''
+    This is the full list:-
+    appCategoryList = ['https://play.google.com/store/apps/category/APP_WALLPAPER','https://play.google.com/store/apps/category/APP_WIDGETS','https://play.google.com/store/apps/category/BOOKS_AND_REFERENCE','https://play.google.com/store/apps/category/BUSINESS','https://play.google.com/store/apps/category/COMICS','https://play.google.com/store/apps/category/COMMUNICATION','https://play.google.com/store/apps/category/EDUCATION','https://play.google.com/store/apps/category/ENTERTAINMENT','https://play.google.com/store/apps/category/FAMILY','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE1','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE2','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE3','https://play.google.com/store/apps/category/FAMILY_ACTION','https://play.google.com/store/apps/category/FAMILY_BRAINGAMES','https://play.google.com/store/apps/category/FAMILY_CREATE','https://play.google.com/store/apps/category/FAMILY_EDUCATION','https://play.google.com/store/apps/category/FAMILY_MUSICVIDEO','https://play.google.com/store/apps/category/FAMILY_PRETEND','https://play.google.com/store/apps/category/FINANCE','https://play.google.com/store/apps/category/GAME','https://play.google.com/store/apps/category/GAME_ACTION','https://play.google.com/store/apps/category/GAME_ADVENTURE','https://play.google.com/store/apps/category/GAME_ARCADE','https://play.google.com/store/apps/category/GAME_BOARD','https://play.google.com/store/apps/category/GAME_CARD','https://play.google.com/store/apps/category/GAME_CASINO','https://play.google.com/store/apps/category/GAME_CASUAL','https://play.google.com/store/apps/category/GAME_EDUCATIONAL','https://play.google.com/store/apps/category/GAME_MUSIC','https://play.google.com/store/apps/category/GAME_PUZZLE','https://play.google.com/store/apps/category/GAME_RACING','https://play.google.com/store/apps/category/GAME_ROLE_PLAYING','https://play.google.com/store/apps/category/GAME_SIMULATION','https://play.google.com/store/apps/category/GAME_SPORTS','https://play.google.com/store/apps/category/GAME_STRATEGY','https://play.google.com/store/apps/category/GAME_TRIVIA','https://play.google.com/store/apps/category/GAME_WORD','https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/LIBRARIES_AND_DEMO','https://play.google.com/store/apps/category/LIFESTYLE','https://play.google.com/store/apps/category/MEDIA_AND_VIDEO','https://play.google.com/store/apps/category/MEDICAL','https://play.google.com/store/apps/category/MUSIC_AND_AUDIO','https://play.google.com/store/apps/category/NEWS_AND_MAGAZINES','https://play.google.com/store/apps/category/PERSONALIZATION','https://play.google.com/store/apps/category/PHOTOGRAPHY','https://play.google.com/store/apps/category/PRODUCTIVITY','https://play.google.com/store/apps/category/SHOPPING','https://play.google.com/store/apps/category/SOCIAL','https://play.google.com/store/apps/category/SPORTS','https://play.google.com/store/apps/category/TOOLS','https://play.google.com/store/apps/category/TRANSPORTATION','https://play.google.com/store/apps/category/TRAVEL_AND_LOCAL','https://play.google.com/store/apps/category/WEATHER']
+    '''
+    ticks = time.time()
+    appMatrixFile = "appMatrix"+str(ticks)+".txt"
+    text_file = open(appMatrixFile, "w")
+    text_file.write("")
+    text_file.close()
+    
+    predictedClustersFile = "predictedClusters"+str(ticks)+".json"
+    text_file = open(predictedClustersFile, "w")
+    text_file.write("")
+    text_file.close()
+    
+    return appMatrixFile, predictedClustersFile, appCategoryList
+
+def main(argv):
+    if len(sys.argv) != 3:
+        sys.stderr.write('Usage: python initClustering.py username api_key\n')
+        sys.exit(1)
+        
+    appMatrixFile, predictedClustersFile = preProcess()
+    
+    startTime = time.time()
+    if appCategoryList[0] == 'top':
+        #Initiate the clustering process for top apps
+        initClusteringTopApps(sys.argv[1], sys.argv[2], predictedClustersFile, appMatrixFile)
+    elif appCategoryList[0] == 'all':
+        #Initiate the clustering process for top apps
+        initClusteringAllApps(sys.argv[1], sys.argv[2], predictedClustersFile, appMatrixFile)
+    else:
+        initClusteringCatApps(sys.argv[1], sys.argv[2], appCategoryList, predictedClustersFile, appMatrixFile)
+    executionTime = str((time.time()-startTime)*1000)
+    print "Execution time was: "+executionTime+" ms"
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
--- code/src/org/ebiquity/data/analysis/selectApps.py
+++ code/src/org/ebiquity/data/analysis/selectApps.py
@@ -14,7 +14,59 @@ import os
 from os.path import isfile, join
 import platform
 
-#No longer necessary to call this
+def createAppDict(dbHandle,sqlStatement):
+    cursor = dbHandle.cursor()
+    appDict = {}
+    try:
+        cursor.execute(sqlStatement)
+        print "Extracting app package names and ids"
+        if cursor.rowcount > 0:
+            queryOutput = cursor.fetchall()
+            for row in queryOutput:
+                appDict[row[1]] = row[0]
+    except:
+        print "Unexpected error in extractAllApps:", sys.exc_info()[0]
+        raise
+    return appDict
+
+def getTopAppsFromDownloadedJSONs():
+    # Detect operating system and takes actions accordingly
+    osInfo = platform.system()
+    currentDirectory = os.getcwd()
+    if osInfo == 'Windows':
+        topAppJsonsFrom42MattersAPIDirectory = currentDirectory+"\\topAppJsonsFrom42MattersAPI"
+    elif osInfo == 'Linux' or osInfo == 'Darwin':
+        topAppJsonsFrom42MattersAPIDirectory = currentDirectory+"/topAppJsonsFrom42MattersAPI"
+    
+    appNameList = []
+    for filename in os.listdir(topAppJsonsFrom42MattersAPIDirectory):
+        topAppDict = json.loads(open(os.path.join(topAppJsonsFrom42MattersAPIDirectory,filename), 'r').read().decode('utf8'))
+        for appData in topAppDict['appList']:
+            if 'package_name' in appData:
+                packageName = str(appData['package_name'])
+                #isDataCollected(dbHandle,packageName)
+                appNameList.append(packageName) 
+
+    return appNameList
+
+# Get a bunch of apps from which you want to get the permissions
+# Select apps which have had their permissions extracted
+def getTopApps(dbHandle):
+    appCategorySQLQueryList = databaseHandler.convertPythonListToSQLQueryList(getTopAppsFromDownloadedJSONs())
+    sqlStatement = "SELECT a.`id`, a.`app_pkg_name` FROM `appdata` a, `appurls` url WHERE a.`app_pkg_name` = url.`app_pkg_name` AND url.`perm_extracted` = 1 AND a.`app_pkg_name` IN ("+appCategorySQLQueryList+");"
+    return createAppDict(dbHandle,sqlStatement)
+
+def getCategoryApps(dbHandle,appCategoryList):
+    appCategorySQLQueryList = databaseHandler.convertPythonListToSQLQueryList(appCategoryList)
+    sqlStatement = "SELECT a.`id`, a.`app_pkg_name` FROM `appdata` a, `appurls` url, `appcategories` cat WHERE a.`app_pkg_name` = url.`app_pkg_name` AND url.`perm_extracted` = 1 AND cat.`url` IN ("+appCategorySQLQueryList+") AND a.`app_category_id` = cat.`id`;"
+    return createAppDict(dbHandle,sqlStatement)
+
+def getAllApps(dbHandle):    
+    sqlStatement = "SELECT a.`id`, a.`app_pkg_name` FROM `appdata` a, `appurls` url WHERE a.`app_pkg_name` = url.`app_pkg_name` AND url.`perm_extracted` = 1;"
+    return createAppDict(dbHandle,sqlStatement)
+
+'''
+No longer necessary to call this
 def isDataCollected(dbHandle,packageName):
     cursor = dbHandle.cursor()
     sqlStatement = "SELECT perm_extracted,parsed FROM `appurls` WHERE `app_pkg_name` = '"+packageName+"';"
@@ -42,100 +94,4 @@ def isDataCollected(dbHandle,packageName):
     except:
         print "Unexpected error in isDataCollected:", sys.exc_info()[0]
         raise
-
-def getAppDict(dbHandle,sqlStatement):
-    cursor = dbHandle.cursor()
-    appDict = {}
-    try:
-        cursor.execute(sqlStatement)
-        print "Extracting app package names and ids"
-        if cursor.rowcount > 0:
-            queryOutput = cursor.fetchall()
-            for row in queryOutput:
-                appDict[row[1]] = row[0]
-    except:
-        print "Unexpected error in extractAllApps:", sys.exc_info()[0]
-        raise
-    return appDict
-
-def getTopAppsFromDownloadedJSONs(dbHandle):
-    # Detect operating system and takes actions accordingly
-    osInfo = platform.system()
-    currentDirectory = os.getcwd()
-    if osInfo == 'Windows':
-        topAppJsonsFrom42MattersAPIDirectory = currentDirectory+"\\topAppJsonsFrom42MattersAPI"
-    elif osInfo == 'Linux' or osInfo == 'Darwin':
-        topAppJsonsFrom42MattersAPIDirectory = currentDirectory+"/topAppJsonsFrom42MattersAPI"
-    
-    appNameList = []
-    for filename in os.listdir(topAppJsonsFrom42MattersAPIDirectory):
-        topAppDict = json.loads(open(os.path.join(topAppJsonsFrom42MattersAPIDirectory,filename), 'r').read().decode('utf8'))
-        for appData in topAppDict['appList']:
-            if 'package_name' in appData:
-                packageName = str(appData['package_name'])
-                #isDataCollected(dbHandle,packageName)
-                appNameList.append(packageName) 
-
-    sqlStatement = "SELECT a.`id`, a.`app_pkg_name` FROM `appdata` a, `appurls` url WHERE a.`app_pkg_name` = url.`app_pkg_name` AND url.`perm_extracted` = 1 AND a.`app_pkg_name` IN ("+databaseHandler.convertPythonListToSQLQueryList(appNameList)+");"
-    return getAppDict(dbHandle,sqlStatement)
-
-def extractAllApps(dbHandle):    
-    sqlStatement = "SELECT a.`id`, a.`app_pkg_name` FROM `appdata` a, `appurls` url WHERE a.`app_pkg_name` = url.`app_pkg_name` AND url.`perm_extracted` = 1;"
-    return getAppDict(dbHandle,sqlStatement)
-
-def extractAppsFromCategory(dbHandle,appCategoryList):
-    appCategorySQLQueryList = databaseHandler.convertPythonListToSQLQueryList(appCategoryList)
-    sqlStatement = "SELECT a.`id`, a.`app_pkg_name` FROM `appdata` a, `appurls` url, `appcategories` cat WHERE a.`app_pkg_name` = url.`app_pkg_name` AND url.`perm_extracted` = 1 AND cat.`url` IN ("+appCategorySQLQueryList+") AND a.`app_category_id` = cat.`id`;"
-    return getAppDict(dbHandle,sqlStatement)
-
-# Get a bunch of apps from which you want to get the permissions
-# Select apps which have had their permissions extracted
-def getCategoryApps(dbHandle,appCategoryList):
-    return extractAppsFromCategory(dbHandle,appCategoryList)
-
-def getAllApps(dbHandle):
-    return extractAllApps(dbHandle)
-
-def getTopApps(dbHandle):
-    return getTopAppsFromDownloadedJSONs(dbHandle)
-
-def preProcess(selectionType):
-    if selectionType == 'top':
-        appCategoryList = ['top']
-    elif selectionType == 'med':
-        appCategoryList = ['https://play.google.com/store/apps/category/MEDICAL']
-    elif selectionType == 'hea':
-        appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS']
-    elif selectionType == 'hmd':
-        appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/MEDICAL']
-    elif selectionType == 'all':
-        appCategoryList = ['all']
-    '''
-    This is the full list:-
-    appCategoryList = ['https://play.google.com/store/apps/category/APP_WALLPAPER','https://play.google.com/store/apps/category/APP_WIDGETS','https://play.google.com/store/apps/category/BOOKS_AND_REFERENCE','https://play.google.com/store/apps/category/BUSINESS','https://play.google.com/store/apps/category/COMICS','https://play.google.com/store/apps/category/COMMUNICATION','https://play.google.com/store/apps/category/EDUCATION','https://play.google.com/store/apps/category/ENTERTAINMENT','https://play.google.com/store/apps/category/FAMILY','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE1','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE2','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE3','https://play.google.com/store/apps/category/FAMILY_ACTION','https://play.google.com/store/apps/category/FAMILY_BRAINGAMES','https://play.google.com/store/apps/category/FAMILY_CREATE','https://play.google.com/store/apps/category/FAMILY_EDUCATION','https://play.google.com/store/apps/category/FAMILY_MUSICVIDEO','https://play.google.com/store/apps/category/FAMILY_PRETEND','https://play.google.com/store/apps/category/FINANCE','https://play.google.com/store/apps/category/GAME','https://play.google.com/store/apps/category/GAME_ACTION','https://play.google.com/store/apps/category/GAME_ADVENTURE','https://play.google.com/store/apps/category/GAME_ARCADE','https://play.google.com/store/apps/category/GAME_BOARD','https://play.google.com/store/apps/category/GAME_CARD','https://play.google.com/store/apps/category/GAME_CASINO','https://play.google.com/store/apps/category/GAME_CASUAL','https://play.google.com/store/apps/category/GAME_EDUCATIONAL','https://play.google.com/store/apps/category/GAME_MUSIC','https://play.google.com/store/apps/category/GAME_PUZZLE','https://play.google.com/store/apps/category/GAME_RACING','https://play.google.com/store/apps/category/GAME_ROLE_PLAYING','https://play.google.com/store/apps/category/GAME_SIMULATION','https://play.google.com/store/apps/category/GAME_SPORTS','https://play.google.com/store/apps/category/GAME_STRATEGY','https://play.google.com/store/apps/category/GAME_TRIVIA','https://play.google.com/store/apps/category/GAME_WORD','https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/LIBRARIES_AND_DEMO','https://play.google.com/store/apps/category/LIFESTYLE','https://play.google.com/store/apps/category/MEDIA_AND_VIDEO','https://play.google.com/store/apps/category/MEDICAL','https://play.google.com/store/apps/category/MUSIC_AND_AUDIO','https://play.google.com/store/apps/category/NEWS_AND_MAGAZINES','https://play.google.com/store/apps/category/PERSONALIZATION','https://play.google.com/store/apps/category/PHOTOGRAPHY','https://play.google.com/store/apps/category/PRODUCTIVITY','https://play.google.com/store/apps/category/SHOPPING','https://play.google.com/store/apps/category/SOCIAL','https://play.google.com/store/apps/category/SPORTS','https://play.google.com/store/apps/category/TOOLS','https://play.google.com/store/apps/category/TRANSPORTATION','https://play.google.com/store/apps/category/TRAVEL_AND_LOCAL','https://play.google.com/store/apps/category/WEATHER']
-    '''
-    return appCategoryList
-
-def main(argv):
-    if len(sys.argv) != 2:
-        sys.stderr.write('Usage: python selectApps.py selectionType\n')
-        sys.exit(1)
-        
-    dbHandle = databaseHandler.dbConnectionCheck() #DB Open
-    startTime = time.time()
-    appCategoryList = preProcess(sys.argv[1])
-    # Get a bunch of apps from which you want to get the permissions
-    # Select apps which have had their permissions extracted
-    if appCategoryList[0] == 'top':
-        appDict = getTopApps(dbHandle)
-    elif appCategoryList[0] == 'all':
-        appDict = getAllApps(dbHandle)
-    else:
-        appDict = getCategoryApps(dbHandle,appCategoryList)
-    executionTime = str((time.time()-startTime)*1000)
-    print "Execution time was: "+executionTime+" ms"
-#     print appDict
-    dbHandle.close() #DB Close
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv))
+'''

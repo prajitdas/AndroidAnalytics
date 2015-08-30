d92729a946c59376bed8aed744c1bfc0c8cc8889
Prajit Kumar Das <prajitdas@gmail.com>
2015-08-30 19:14:00 -0400

    run clusterign and plot results

--- code/src/org/ebiquity/data/analysis/runClustering.py
+++ code/src/org/ebiquity/data/analysis/runClustering.py
@@ -22,6 +22,7 @@ import cPickle
 import weightedJaccardSimilarity as wjs
 from sklearn.decomposition import TruncatedSVD
 import os
+import matplotlib.pyplot as plt
 
 def writeMatrixToFile(appMatrix, appMatrixFile):
     #Once the whole matrix is created then dump to a file
@@ -102,6 +103,42 @@ def doJaccard(username, api_key, appCategoryListSelection, predictedClustersFile
 #        SpectralClusteringObject = SpectralClustering(n_clusters=numberOfClusters)#,affinity='precomputed')
 #        clusterLabelsAssigned = SpectralClusteringObject.fit_predict(X)
         
+        #Plotting results
+        # Step size of the mesh. Decrease to increase the quality of the VQ.
+        h = .02     # point in the mesh [x_min, m_max]x[y_min, y_max].
+        
+        # Plot the decision boundary. For that, we will assign a color to each
+        x_min, x_max = X[:, 0].min() + 1, X[:, 0].max() - 1
+        y_min, y_max = X[:, 1].min() + 1, X[:, 1].max() - 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        
+        # Obtain labels for each point in mesh. Use last trained model.
+        Z = KMeansObject.predict(np.c_[xx.ravel(), yy.ravel()])
+        
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.figure(1)
+        plt.clf()
+        plt.imshow(Z, interpolation='nearest',
+                   extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+                   cmap=plt.cm.Paired,
+                   aspect='auto', origin='lower')
+        
+        plt.plot(X[:, 0], X[:, 1], 'k.', markersize=2)
+        # Plot the centroids as a white X
+        centroids = KMeansObject.cluster_centers_
+        plt.scatter(centroids[:, 0], centroids[:, 1],
+                    marker='x', s=169, linewidths=3,
+                    color='w', zorder=10)
+        plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+                  'Centroids are marked with white cross')
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
+        plt.xticks(())
+        plt.yticks(())
+        plt.show()
+        
+        #Silhouette Evaluation starts
         counter = 0
         predictedClusters = {}
         for appName in appVector:

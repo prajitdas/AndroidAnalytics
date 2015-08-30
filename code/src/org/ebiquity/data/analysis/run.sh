4b4b806250d60cdcdcba5c92d30cd9150fdfc755
Prajit Kumar Das <prajitdas@gmail.com>
2015-08-30 18:47:31 -0400

    run options changed

--- code/src/org/ebiquity/data/analysis/run.sh
+++ code/src/org/ebiquity/data/analysis/run.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-python permissionsClustering.py prajitdas 15irk8yvf3
-'med'
-'hea'
-'hmd'
-'top'
-'all'
-
-'int'
-'top25'
-'google'
+python initClustering.py prajitdas 15irk8yvf3 hmdtop '' allow

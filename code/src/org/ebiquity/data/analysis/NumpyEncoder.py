7606af56dc2f8ff2ebf175e14ef6d4203eb5c646
Prajit Kumar Das <prajitdas@gmail.com>
2015-09-13 21:04:20 -0400

    bug removed

--- code/src/org/ebiquity/data/analysis/NumpyEncoder.py
+++ code/src/org/ebiquity/data/analysis/NumpyEncoder.py
@@ -4,11 +4,17 @@ import numpy as np
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
-        """
-        if input object is a ndarray it will be converted into a dict holding dtype, shape and the data base64 encoded
+        """If input object is an ndarray it will be converted into a dict 
+        holding dtype, shape and the data, base64 encoded.
         """
         if isinstance(obj, np.ndarray):
-            data_b64 = base64.b64encode(obj.data)
+            if obj.flags['C_CONTIGUOUS']:
+                obj_data = obj.data
+            else:
+                cont_obj = np.ascontiguousarray(obj)
+                assert(cont_obj.flags['C_CONTIGUOUS'])
+                obj_data = cont_obj.data
+            data_b64 = base64.b64encode(obj_data)
             return dict(__ndarray__=data_b64,
                         dtype=str(obj.dtype),
                         shape=obj.shape)

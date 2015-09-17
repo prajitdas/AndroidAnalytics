5660aaf551a7ccd0044c6dc055d3b7d11b2d2a4e
Prajit Kumar Das <prajitdas@gmail.com>
2015-09-16 20:50:10 -0400

    adding plot results

--- code/src/org/ebiquity/data/analysis/plot.sh
+++ code/src/org/ebiquity/data/analysis/plot.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-rm predictedClusters*.json appMatrix*.txt
-#python initClustering.py prajitdas 15irk8yvf3 hmdtop '' allow
 python plotResults.py prajitdas 15irk8yvf3 predictedClusters14422364380.Loop190.json

b72c59115dc425d3391e738358159552486728d9
Prajit Kumar Das <prajitdas@gmail.com>
2015-10-02 18:41:49 -0400

    word2vec

--- code/src/org/ebiquity/data/appMetadataAnalysis/appDescriptionsClustering/initWord2Vec.py
+++ code/src/org/ebiquity/data/appMetadataAnalysis/appDescriptionsClustering/initWord2Vec.py
@@ -15,6 +15,25 @@ import time
 import databaseHandler
 import selectAppDescriptions as sad
 import json
+import numpy as np
+import lda
+import lda.datasets
+
+def lda():
+    X = lda.datasets.load_reuters()
+    vocab = lda.datasets.load_reuters_vocab()
+    titles = lda.datasets.load_reuters_titles()
+    X.shape
+    (395, 4258)
+    X.sum()
+    84010
+    model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
+    model.fit(X)  # model.fit_transform(X) is also available
+    topic_word = model.topic_word_  # model.components_ also works
+    n_top_words = 8
+    for i, topic_dist in enumerate(topic_word):
+        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
 def getPath():
     if socket.gethostname() == 'eclipse':

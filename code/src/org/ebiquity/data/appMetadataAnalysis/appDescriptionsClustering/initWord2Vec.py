7774eff13f6b19968a355563ee5cf05a3091043d
Prajit Kumar Das <prajitdas@gmail.com>
2015-10-05 15:32:19 -0400

    LDA

--- code/src/org/ebiquity/data/appMetadataAnalysis/appDescriptionsClustering/initWord2Vec.py
+++ code/src/org/ebiquity/data/appMetadataAnalysis/appDescriptionsClustering/initWord2Vec.py
@@ -39,7 +39,6 @@ def exampleLDAExecution():
 	print("type(titles): {}".format(type(titles)))
 	print("len(titles): {}\n".format(len(titles)))
 
-
 	doc_id = 0
 	word_id = 3117
 
@@ -47,19 +46,19 @@ def exampleLDAExecution():
 	print("-- count: {}".format(X[doc_id, word_id]))
 	print("-- word : {}".format(vocab[word_id]))
 	print("-- doc  : {}".format(titles[doc_id]))
-	
-	# model = LDA(n_topics=20, n_iter=1500, random_state=1)
-	# model.fit(X)  # model.fit_transform(X) is also available
-	
-	# topic_word = model.topic_word_  # model.components_ also works
-	# n_top_words = 8
-	# for i, topic_dist in enumerate(topic_word):
-	# 	topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
-	# 	print('Topic {}: {}'.format(i, ' '.join(topic_words)))
 
-	# doc_topic = model.doc_topic_
-	# for i in range(10):
-	# 	print("{} (top topic: {})".format(titles[i], doc_topic[i].argmax()))
+	model = LDA(n_topics=20, n_iter=500, random_state=1)
+	model.fit(X)  # model.fit_transform(X) is also available
+
+	topic_word = model.topic_word_  # model.components_ also works
+	n_top_words = 10
+	for i, topic_dist in enumerate(topic_word):
+		topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+		print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+
+	doc_topic = model.doc_topic_
+	for i in range(10):
+		print("{} (top topic: {})".format(titles[i], doc_topic[i].argmax()))
 
 def appDescriptionsLDA():
 	X = data.load_reuters()
@@ -72,11 +71,11 @@ def appDescriptionsLDA():
 
 	X.shape
 	X.sum()
-	model = LDA(n_topics=20, n_iter=1500, random_state=1)
+	model = LDA(n_topics=20, n_iter=500, random_state=1)
 	model.fit(X)  # model.fit_transform(X) is also available
 	
 	topic_word = model.topic_word_  # model.components_ also works
-	n_top_words = 8
+	n_top_words = 10
 	for i, topic_dist in enumerate(topic_word):
 		topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
 		print('Topic {}: {}'.format(i, ' '.join(topic_words)))
@@ -128,35 +127,35 @@ def initWord2Vec(appCategoryList, appCategoryListSelection, appDescriptionsFile)
 	dbHandle.close() #DB Close
 
 def preProcess(appCategoryListSelection):
-    if appCategoryListSelection == 'med':
-        appCategoryList = ['https://play.google.com/store/apps/category/MEDICAL']
-    elif appCategoryListSelection == 'hea':
-        appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS']
-    elif appCategoryListSelection == 'hmd':
-        appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/MEDICAL']
-    elif appCategoryListSelection == 'hmdtop':
-        appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/MEDICAL']
-    elif appCategoryListSelection == 'fabra':
-        appCategoryList = ['https://play.google.com/store/apps/category/FAMILY_BRAINGAMES']
-    elif appCategoryListSelection == 'top':
-        appCategoryList = ['top']
-    elif appCategoryListSelection == 'all':
-        appCategoryList = ['all']
-    elif appCategoryListSelection == 'cattop':
-        appCategoryList = ['cattop']
-    '''
+	if appCategoryListSelection == 'med':
+		appCategoryList = ['https://play.google.com/store/apps/category/MEDICAL']
+	elif appCategoryListSelection == 'hea':
+		appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS']
+	elif appCategoryListSelection == 'hmd':
+		appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/MEDICAL']
+	elif appCategoryListSelection == 'hmdtop':
+		appCategoryList = ['https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/MEDICAL']
+	elif appCategoryListSelection == 'fabra':
+		appCategoryList = ['https://play.google.com/store/apps/category/FAMILY_BRAINGAMES']
+	elif appCategoryListSelection == 'top':
+		appCategoryList = ['top']
+	elif appCategoryListSelection == 'all':
+		appCategoryList = ['all']
+	elif appCategoryListSelection == 'cattop':
+		appCategoryList = ['cattop']
+	'''
     This is the full list:-
     appCategoryList = ['https://play.google.com/store/apps/category/APP_WALLPAPER','https://play.google.com/store/apps/category/APP_WIDGETS','https://play.google.com/store/apps/category/BOOKS_AND_REFERENCE','https://play.google.com/store/apps/category/BUSINESS','https://play.google.com/store/apps/category/COMICS','https://play.google.com/store/apps/category/COMMUNICATION','https://play.google.com/store/apps/category/EDUCATION','https://play.google.com/store/apps/category/ENTERTAINMENT','https://play.google.com/store/apps/category/FAMILY','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE1','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE2','https://play.google.com/store/apps/category/FAMILY?age=AGE_RANGE3','https://play.google.com/store/apps/category/FAMILY_ACTION','https://play.google.com/store/apps/category/FAMILY_BRAINGAMES','https://play.google.com/store/apps/category/FAMILY_CREATE','https://play.google.com/store/apps/category/FAMILY_EDUCATION','https://play.google.com/store/apps/category/FAMILY_MUSICVIDEO','https://play.google.com/store/apps/category/FAMILY_PRETEND','https://play.google.com/store/apps/category/FINANCE','https://play.google.com/store/apps/category/GAME','https://play.google.com/store/apps/category/GAME_ACTION','https://play.google.com/store/apps/category/GAME_ADVENTURE','https://play.google.com/store/apps/category/GAME_ARCADE','https://play.google.com/store/apps/category/GAME_BOARD','https://play.google.com/store/apps/category/GAME_CARD','https://play.google.com/store/apps/category/GAME_CASINO','https://play.google.com/store/apps/category/GAME_CASUAL','https://play.google.com/store/apps/category/GAME_EDUCATIONAL','https://play.google.com/store/apps/category/GAME_MUSIC','https://play.google.com/store/apps/category/GAME_PUZZLE','https://play.google.com/store/apps/category/GAME_RACING','https://play.google.com/store/apps/category/GAME_ROLE_PLAYING','https://play.google.com/store/apps/category/GAME_SIMULATION','https://play.google.com/store/apps/category/GAME_SPORTS','https://play.google.com/store/apps/category/GAME_STRATEGY','https://play.google.com/store/apps/category/GAME_TRIVIA','https://play.google.com/store/apps/category/GAME_WORD','https://play.google.com/store/apps/category/HEALTH_AND_FITNESS','https://play.google.com/store/apps/category/LIBRARIES_AND_DEMO','https://play.google.com/store/apps/category/LIFESTYLE','https://play.google.com/store/apps/category/MEDIA_AND_VIDEO','https://play.google.com/store/apps/category/MEDICAL','https://play.google.com/store/apps/category/MUSIC_AND_AUDIO','https://play.google.com/store/apps/category/NEWS_AND_MAGAZINES','https://play.google.com/store/apps/category/PERSONALIZATION','https://play.google.com/store/apps/category/PHOTOGRAPHY','https://play.google.com/store/apps/category/PRODUCTIVITY','https://play.google.com/store/apps/category/SHOPPING','https://play.google.com/store/apps/category/SOCIAL','https://play.google.com/store/apps/category/SPORTS','https://play.google.com/store/apps/category/TOOLS','https://play.google.com/store/apps/category/TRANSPORTATION','https://play.google.com/store/apps/category/TRAVEL_AND_LOCAL','https://play.google.com/store/apps/category/WEATHER']
     '''
 
-    ticks = time.time()
-    uniformString = str(ticks).replace('.', '')
-    appDescriptionsFile = getPath()+'appDescription'+uniformString+'.json'
-    text_file = open(appDescriptionsFile, 'w')
-    text_file.write('')
-    text_file.close()
+	ticks = time.time()
+	uniformString = str(ticks).replace('.', '')
+	appDescriptionsFile = getPath()+'appDescription'+uniformString+'.json'
+	text_file = open(appDescriptionsFile, 'w')
+	text_file.write('')
+	text_file.close()
 
-    return appCategoryList, appDescriptionsFile
+	return appCategoryList, appDescriptionsFile
 
 def main(argv):
     if len(sys.argv) != 2:

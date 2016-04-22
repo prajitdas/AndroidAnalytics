6be453a8677b58f2e67caab7ee5d04fbe6aa8ca5
Prajit Kumar Das <prajitdas@gmail.com>
2016-04-22 05:30:05 -0400

    format fixing

--- code/src/org/ebiquity/data/systemCallAnalysis/testOneApp.sh
+++ code/src/org/ebiquity/data/systemCallAnalysis/testOneApp.sh
@@ -1,16 +1,7 @@
-#!/bin/sh
 # Apps to test
 # syscall stats using
 # cat <app_pkg_name>.out  | cut -f1 -d"(" | sort | uniq -c | sort -r
 # get permissions using
 # aapt d permissions <app_apk_file_name>
 
-value=`cat apkconfig.ini | grep apkLocation | cut -f2 -d'='`
-
-echo $value
-
-if [[ $value != *[* ]]; then
-	echo "I came here!"
-else
-	echo "fix the config file"
-fi
+sh automatingStrace.sh com.saranomy.flashlightHacked.apk

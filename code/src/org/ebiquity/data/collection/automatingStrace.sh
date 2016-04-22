e9c6771d4fbec39266e6dda45c3216152da448fa
Prajit Kumar Das <prajitdas@gmail.com>
2016-04-22 04:10:15 -0400

    ls -lrt

---
+++ code/src/org/ebiquity/data/collection/automatingStrace.sh
@@ -0,0 +1,34 @@
+# Install the app
+adb install $1
+
+# Extract package and launcher activity information
+package=$(aapt dump badging $1|awk -F" " '/package/ {print $2}'|awk -F"'" '/name=/ {print $2}')
+activity=$(aapt dump badging $1|awk -F" " '/launchable-activity/ {print $2}'|awk -F"'" '/name=/ {print $2}')
+
+# Start package using ActivityManager in order to determine the process Id of the app
+adb shell am start -n $package/$activity
+processId=$(adb shell ps | awk -v pattern="$package" -F" " '$0 ~ pattern { print $2 }')
+outputFile=$(echo "/sdcard/"$package".out")
+# Verifying the variables (for Debug)
+echo $package
+echo $activity
+echo $processId
+echo $outputFile
+
+# Starting the trace process on the app's process id. This is assuming that we have the root shell
+adb shell "nohup strace -p $processId -o $outputFile &> /sdcard/nohup.out&"
+
+# Using monkey to generate a certain number of pseudo-random events
+adb shell monkey -p $package -v 100 > "$package"monkey.out
+# adb shell monkey -p $package --pct-touch 95 -v 100 > "$package"monkey.out
+# adb shell monkey -p $package -c android.intent.category.LAUNCHER 100
+
+# strace is still running so we just make a copy of the out file
+# straceOutFilePath=$(echo "/sdcard/"$package"Strace.out")
+# adb shell "cp $outputFile $straceOutFilePath"
+
+# Uninstall the app
+adb uninstall $package
+
+# Extract the out file containing the output of strace
+adb pull $outputFile
---
+++ code/src/org/ebiquity/data/collection/createAVD.sh
@@ -0,0 +1,1 @@
+android create avd -n nexus6 -t 23 --skin WVGA800 --abi default/x86_64

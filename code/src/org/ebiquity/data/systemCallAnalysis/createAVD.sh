abe4568bcffa65f5b3f34031283158022923e578
Prajit Kumar Das <prajitdas@gmail.com>
2016-04-25 11:33:35 -0400

    fixed the issue with target of AVD

--- code/src/org/ebiquity/data/systemCallAnalysis/createAVD.sh
+++ code/src/org/ebiquity/data/systemCallAnalysis/createAVD.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-android create avd -n nexus6 -t 23 --skin WVGA800 --abi default/x86_64
+if [ "$1" != "" ];
+then
+	android create avd -n nexus6 -t $1 --skin WVGA800 --abi default/x86_64
+else
+	echo "Run the command 'android list targets' and determine the android sdk version 23 target id and use that as an input argument."
+	echo "Can't create an AVD without that!"
+fi

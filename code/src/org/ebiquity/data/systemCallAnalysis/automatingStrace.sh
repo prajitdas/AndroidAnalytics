d0d1e09b592af7a6758f402bccc4cd10a5f170c7
Prajit Kumar Das <prajitdas@gmail.com>
2016-04-25 19:03:21 -0400

    python script added still somethings are TBD

--- code/src/org/ebiquity/data/systemCallAnalysis/automatingStrace.sh
+++ code/src/org/ebiquity/data/systemCallAnalysis/automatingStrace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install the app
-adb install $1syscallAnalysis
+adb install $1
 
 # Extract package and launcher activity information
 package=$(aapt dump badging $1|awk -F" " '/package/ {print $2}'|awk -F"'" '/name=/ {print $2}')
--- code/src/org/ebiquity/data/systemCallAnalysis/syscallAnalysis.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/syscallAnalysis.py
@@ -13,32 +13,71 @@ import sys
 import os
 import platform
 from ConfigParser import SafeConfigParser
+import subprocess as s
 
-from code.src.org.ebiquity.data.collection.GooglePlayAPI.androguard.androproto import apk
-
-
-def getApkPath():
+def getApkFolderPath():
     parser = SafeConfigParser()
     parser.read('apkconfig.ini')
 
-    path = parser.get('apkLocation', 'defaultPath')
+    path = parser.get('apkconfig', 'apkLocation')
     return path
 
-def doTask():
-    osInfo = platform.system()
-    currentPath = os.getcwd()
-    apkPath = getApkPath()
-    if apkPath != "defaultPath":
-        if osInfo == 'Windows':
-            outputDirectoryPath = currentPath + "\\out"
-        elif osInfo == 'Linux' or 'Darwin':
-            outputDirectoryPath = currentPath + "/out"
-        else:
-            print 'The current os not supported at the moment.'
-            return
-    else:
-        print "fix the config file"
+def findAllFilesWithExtension(root, ext):
+    files = os.listdir(root)
+    apkDict = {}
+    for file in files:
+        if file.endswith(ext):
+            apkDict[file] = os.path.join(root,file)
+    return apkDict
 
+def getOutputDirectoryPath(currentPath):
+    osInfo = platform.system()
+    if osInfo == 'Windows':
+        outputDirectoryPath = currentPath + "\\out"
+    elif osInfo == 'Linux' or 'Darwin':
+        outputDirectoryPath = currentPath + "/out"
+    else:
+        print "The current OS is not supported at the moment. Try Windows, Linux or OS X."
+        sys.exit(1)
+    return outputDirectoryPath
+
+def isBootAnimationComplete():
+    time.sleep(30)
+    cmd = 'adb shell getprop init.svc.bootanim'
+    output = s.check_output(cmd.split()).split('\r\n')
+    return output[0]
+
+def runExperimentsOnEmulator(currentPath, apkFolderPath, outputDirectoryPath, apkDict):
+    for key in apkDict.keys():
+        # For each app execution start emulator for AVD nexus6, in wiped mode.
+        # Make sure you have created the AVD first.
+        emulatorStartCmd = 'bash startEmulator.sh'
+        s.call(emulatorStartCmd.split())
+        while True:
+            result = "emptyString"
+            result = isBootAnimationComplete()
+            print result
+            if result == "":
+                print "Something went wrong, probably can't start emulator for some reason! \nCheck if AVD exists or not. Or maybe something else is wrong. Check output of 'sudo kvm-ok'"
+            elif result == "stopped":
+                print "AVD is ready"
+                #bash automatingStrace.sh $file
+                break;
+            else:
+                print "Still waiting for emulator to finish booting!"
+                time.sleep(30)
+        s.call ['adb', 'emu', 'kill']
+
+def doTask():
+    currentPath = os.getcwd()
+    apkFolderPath = getApkFolderPath()
+    outputDirectoryPath = getOutputDirectoryPath(currentPath)
+    if apkFolderPath != "defaultPath":
+        apkDict = findAllFilesWithExtension(apkFolderPath, '.apk')
+    else:
+        print "fix the config file\n"
+        return
+    runExperimentsOnEmulator(currentPath, apkFolderPath, outputDirectoryPath, apkDict)
 
 def main(argv):
 	if len(sys.argv) != 1:

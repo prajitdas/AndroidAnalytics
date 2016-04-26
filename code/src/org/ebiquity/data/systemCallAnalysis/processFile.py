ee3f6330c454fc3ef64778acdf05a0fc68d64b11
Prajit Kumar Das <prajitdas@gmail.com>
2016-04-26 09:52:21 -0400

    master json file creation works

--- code/src/org/ebiquity/data/systemCallAnalysis/processFile.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/processFile.py
@@ -12,19 +12,40 @@ import time
 import sys
 import os
 import fnmatch as fm
+from collections import OrderedDict as od
+import json
 
-def processFile(file,appPkgName):
-	print "Processing file: "+file
+def processFile(filePath):
+	syscallDict = {}
+	with open(filePath, 'r') as fp:
+		for line in fp:
+			syscall = line.split('(')[0].strip()
+			if syscall in syscallDict:
+				syscallDict[syscall] += 1
+			else:
+				syscallDict[syscall] = 1
+	# for i, (k,v) in enumerate(od(sorted(syscallDict.items(), key=lambda k:k[1], reverse=True)).iteritems()):
+	# 	print k,v
+	return syscallDict
 
-def extractFeatures(root,appPkgName):
+def extractFeatures(root,appPkgName,masterJsonFile):
 	appOutputFolder = os.path.join(root,appPkgName)
-	for file in appOutputFolder:
+	syscallDict = {}
+	for file in os.listdir(appOutputFolder):
 		if not fm.fnmatch(file, '*monkey.out'):
-			processFile(file,appPkgName)
+			syscallDict = processFile(os.path.join(appOutputFolder,file))
+	try:
+		jsonDict = json.loads(open(masterJsonFile).read())
+	except:
+		jsonDict = {}
+	jsonDict[appPkgName] = syscallDict
+	open(masterJsonFile,"w").write(json.dumps(jsonDict))
 
 def doTask():
+	masterJsonFile = "/Users/prajit/Work/AndroidAnalytics/code/src/org/ebiquity/data/systemCallAnalysis/masterJsonOutputFile.json"
 	outDir = "/Users/prajit/Work/AndroidAnalytics/code/src/org/ebiquity/data/systemCallAnalysis/out"
 	appPkgName = "com.rvappstudios.flashlight"
+	extractFeatures(outDir,appPkgName,masterJsonFile)
 
 def main(argv):
 	if len(sys.argv) != 1:

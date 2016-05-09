83db520ef0df32221d61e507e7254251113dd987
Prajit Kumar Das <prajitdas@gmail.com>
2016-05-09 12:17:13 -0400

    42 matters new data

--- code/src/org/ebiquity/data/appMetadataAnalysis/permissionsClustering/getData42MattersApi.sh
+++ code/src/org/ebiquity/data/appMetadataAnalysis/permissionsClustering/getData42MattersApi.sh
@@ -1,49 +1,93 @@
 #!/bin/bash
 #Data collected on May 10, 2016
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=APPS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/APPS.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAMES&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAMES.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=BOOKS_AND_REFERENCE&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/BOOKS_AND_REFERENCE.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=BUSINESS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/BUSINESS.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=COMICS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/COMICS.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=COMMUNICATION&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/COMMUNICATION.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=EDUCATION&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/EDUCATION.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=ENTERTAINMENT&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/ENTERTAINMENT.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=FINANCE&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/FINANCE.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=HEALTH_AND_FITNESS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/HEALTH_AND_FITNESS.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=LIFESTYLE&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/LIFESTYLE.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=MEDIA_AND_VIDEO&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/MEDIA_AND_VIDEO.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=MEDICAL&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/MEDICAL.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=MUSIC_AND_AUDIO&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/MUSIC_AND_AUDIO.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=NEWS_AND_MAGAZINES&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/NEWS_AND_MAGAZINES.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=PERSONALIZATION&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/PERSONALIZATION.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=PHOTOGRAPHY&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/PHOTOGRAPHY.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=PRODUCTIVITY&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/PRODUCTIVITY.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=SHOPPING&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/SHOPPING.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=SOCIAL&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/SOCIAL.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=SPORTS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/SPORTS.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=TOOLS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/TOOLS.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=TRANSPORTATION&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/TRANSPORTATION.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=TRAVEL_AND_LOCAL&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/TRAVEL_AND_LOCAL.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=WEATHER&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/WEATHER.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=LIBRARIES_AND_DEMO&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/LIBRARIES_AND_DEMO.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_ARCADE&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_ARCADE.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_PUZZLE&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_PUZZLE.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_CARD&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_CARD.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_CASUAL&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_CASUAL.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_RACING&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_RACING.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_SPORTS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_SPORTS.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_ACTION&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_ACTION.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_ADVENTURE&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_ADVENTURE.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_BOARD&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_BOARD.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_CASINO&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_CASINO.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_EDUCATIONAL&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_EDUCATIONAL.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_FAMILY&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_FAMILY.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_MUSIC&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_MUSIC.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_ROLE_PLAYING&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_ROLE_PLAYING.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_SIMULATION&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_SIMULATION.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_STRATEGY&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_STRATEGY.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_TRIVIA&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_TRIVIA.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=GAME_WORD&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/GAME_WORD.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=APP_WALLPAPER&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/APP_WALLPAPER.json
+sleep 10
 wget "https://42matters.com/api/1/apps/top_google_charts.json?list_name=topselling_free&cat_key=APP_WIDGETS&country=US&limit=10&access_token=4b97833427d3533050e6863a6f182c22738d3823" -O topAppJsonsFrom42MattersAPI/APP_WIDGETS.json
-

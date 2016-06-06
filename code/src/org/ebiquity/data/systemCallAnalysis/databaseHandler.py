d74410cf40cff3ba7ae30b97c500e094d08471e7
Prajit Kumar Das <prajitdas@gmail.com>
2016-06-06 12:11:32 -0400

    merging

--- code/src/org/ebiquity/data/systemCallAnalysis/databaseHandler.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/databaseHandler.py
@@ -7,7 +7,9 @@ from ConfigParser import SafeConfigParser
 import MySQLdb
 import _mysql_exceptions
 import sys
+import time
 import logging
+import mysql
 logging.basicConfig(filename='syscall.log',level=logging.DEBUG)
 # Fire an DML SQL statement and commit data
 def dbManipulateData(dbHandle, sqlStatement):
@@ -50,3 +52,34 @@ def convertPythonListToSQLQueryList(pythonList):
 		return ''#List was empty return empty string
 	else:
 		return '\''+'\',\''.join(pythonList)+'\''#Joining the Strings in the list to each other for use in SQL IN LIST query
+
+def test():
+	dbHandle = dbConnectionCheck() #DB Open
+	cursor = dbHandle.cursor()
+	
+	sqlStatement = "SHOW TABLES;"
+	try:
+		cursor.execute(sqlStatement)
+		print "Got"+cursor.rowcount+"rows"
+		if cursor.rowcount > 0:
+			queryOutput = cursor.fetchall()
+			for row in queryOutput:
+				print row[0]
+	except:
+		logging.debug('Unexpected error in test:'+sys.exc_info()[0])
+		raise
+	
+	dbHandle.close() #DB Close
+
+def main(argv):
+	if len(sys.argv) != 1:
+		sys.stderr.write('Usage: python databaseHandler.py\n')
+		sys.exit(1)
+
+	startTime = time.time()
+	test()
+	executionTime = str((time.time()-startTime)*1000)
+	logging.debug('Execution time was: '+executionTime+' ms')
+
+if __name__ == "__main__":
+	sys.exit(main(sys.argv))

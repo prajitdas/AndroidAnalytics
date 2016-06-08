03fbd07da5d5c7f1ad3d5be0d72736765913e1b8
Prajit Kumar Das <prajitdas@gmail.com>
2016-06-07 21:19:05 -0400

    fixing clustering distance problems

--- code/src/org/ebiquity/data/systemCallAnalysis/runClustering.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/runClustering.py
@@ -92,7 +92,7 @@ def doCluster(username, api_key, appMatrixFile, predictedClustersFile, jsonDict,
 		#doScatterPlot(X, numberOfClusters, KMeansObject)
 		SpectralClusteringObject = SpectralClustering(n_clusters=numberOfClusters)#, eigen_solver='arpack')#, assign_labels='discretize')#, affinity='precomputed')
 		clusterLabelsAssigned = SpectralClusteringObject.fit_predict(X)
-		centroids = KMeansObject.cluster_centers_
+		centroids = SpectralClusteringObject.cluster_centers_
 
 		#Silhouette Evaluation starts
 		counter = 0

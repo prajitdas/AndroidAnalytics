c234982ba41653b98d09afc3a018beedd7439c94
Prajit Kumar Das <prajitdas@gmail.com>
2016-06-08 10:00:57 -0400

    running on real devices

--- code/src/org/ebiquity/data/systemCallAnalysis/syscallAnalysis.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/syscallAnalysis.py
@@ -44,16 +44,16 @@ def getOutputDirectoryPath(currentPath):
 		sys.exit(1)
 	return outputDirectoryPath
 
-def runExperimentsOnEmulator(username,api_key,currentPath,apkFolderPath,outputDirectoryPath,apkDict):
+def runExperiments(username,api_key,currentPath,apkFolderPath,outputDirectoryPath,apkDict,realOrFake):
 	for key in apkDict.keys():
-		logging.debug('Working on runExperimentsOnEmulator for the app: '+apkDict[key])
+		logging.debug('Working on runExperiments for the app: '+apkDict[key])
 		# For each app execution start emulator for AVD nexus6,in wiped mode.
 		# Make sure you have created the AVD first.
 		emulatorStartCmd = 'bash startEmulator.sh'
 		s.call(emulatorStartCmd.split())
 		# Executing the test scenario for Android monkey for a particular app apk
 		try:
-			exAndMon.executeTestScenarioForAndroidMonkey(apkDict[key])
+			exAndMon.executeTestScenarioForAndroidMonkey(apkDict[key],realOrFake)
 		except exAndMon.RunExpException:
 			logging.debug('Experiments failed for: '+apkDict[key])
 			# Experiments are not working delete the apk name from the execution list
@@ -66,15 +66,15 @@ def runExperimentsOnEmulator(username,api_key,currentPath,apkFolderPath,outputDi
 		emulatorKillCmd = 'bash killEmulator.sh'
 		s.call(emulatorKillCmd.split())
 		# At this point we have to process the results and extract the features of an app,to run ml algorithms later.
-		logging.debug('Finished running experiments, extracting features for the app: '+key)
-		pf.extractFeatures(currentPath,outputDirectoryPath,key)
-		logging.debug('Done with extracing features for the app: '+key+' onto the next app!')
-		time.sleep(120)
+		# logging.debug('Finished running experiments, extracting features for the app: '+key)
+		# pf.extractFeatures(currentPath,outputDirectoryPath,key)
+		# logging.debug('Done with extracing features for the app: '+key+' onto the next app!')
+		# time.sleep(120)
 
 	# After all the apps have been processed and features extracted,we may run the ML algos.
 	#initCl.initClustering(username,api_key,currentPath)
 
-def doTask(username,api_key):
+def doTask(username,api_key,realOrFake):
 	currentPath = os.getcwd()
 	apkFolderPath = getApkFolderPath()
 	if '[' in apkFolderPath:
@@ -83,18 +83,20 @@ def doTask(username,api_key):
 	else:
 		apkDict = findAllFilesWithExtension(apkFolderPath,'.apk')
 	outputDirectoryPath = getOutputDirectoryPath(currentPath)
-	runExperimentsOnEmulator(username,api_key,currentPath,apkFolderPath,outputDirectoryPath,apkDict)
+	runExperiments(username,api_key,currentPath,apkFolderPath,outputDirectoryPath,apkDict,realOrFake)
+
 
 def main(argv):
-	if len(sys.argv) != 3:
-		sys.stderr.write('Usage: python syscallAnalysis.py username api_key\n')
+	if len(sys.argv) != 4:
+		sys.stderr.write('Usage: python syscallAnalysis.py username api_key realOrFake\n')
 		sys.exit(1)
 
 	username = sys.argv[1]
 	api_key = sys.argv[2]
+	realOrFake = sys.argv[3]
 
 	startTime = time.time()
-	doTask(username,api_key)
+	doTask(username,api_key,realOrFake)
 	executionTime = str((time.time()-startTime)*1000)
 	logging.debug('Execution time was: '+executionTime+' ms')
 

e18b5bdf57b34e3f72d70dbd2db8eceec59fee30
Prajit Kumar Das <prajitdas@gmail.com>
2016-06-16 22:24:43 -0400

    avd config change

--- code/src/org/ebiquity/data/systemCallAnalysis/createAVD.sh
+++ code/src/org/ebiquity/data/systemCallAnalysis/createAVD.sh
@@ -3,9 +3,9 @@ if [ "$1" != "" ];
 then
 	if [ `hostname` == "eclipse" ];
 	then
-		android create avd -n nexus6 -t $1 --skin WVGA800 --abi default/x86_64 -p $ANDROID_SDK_HOME/.android/avd --force
+		android create avd -n nexus6 -t $1 --skin WVGA800 --abi google_apis/x86_64 -p $ANDROID_SDK_HOME/.android/avd --force
 	else
-		android create avd -n nexus6 -t $1 --skin WVGA800 --abi default/x86_64
+		android create avd -n nexus6 -t $1 --skin WVGA800 --abi google_apis/x86_64
 	fi
 else
 	echo "Run the command 'android list targets' and determine the android sdk version 23 target id and use that as an input argument."

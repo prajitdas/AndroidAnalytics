ae2440a7d1d2537b8ae77955f69f479146d49651
Prajit Kumar Das <prajitdas@gmail.com>
2016-06-22 20:48:59 -0400

    To update the appdata table with the correct review_rating or  setting still_in_googleplaystore = 0

---
+++ code/src/org/ebiquity/data/collection/updateReviewRatings.py
@@ -0,0 +1,84 @@
+'''
+Created on June 22, 2016
+@author: Prajit Kumar Das
+Usage: python updateReviewRatings.py
+Purpose: To update the appdata table with the correct review_rating or 
+setting still_in_googleplaystore = 0 
+if the app is no longer available on the play store (Error 404)
+or if the app is not available in the current location context on the play store (Error 403)
+'''
+
+import mysql.connector as mysql
+import sys
+import urllib2
+from bs4 import BeautifulSoup as bs
+
+# Fire an DML SQL statement and commit data
+def dbManipulateData(dbHandle, sqlStatement):
+	cursor = dbHandle.cursor()
+	try:
+		cursor.execute('SET NAMES utf8;')
+		cursor.execute('SET CHARACTER SET utf8;')
+		cursor.execute('SET character_set_connection=utf8;')
+		cursor.execute(sqlStatement)
+		dbHandle.commit()
+	except mysql.errors.IntegrityError as err:
+		logging.debug('data present'.format(err))
+		return -1
+	except mysql.errors.ProgrammingError as err:
+		logging.debug('ProgrammingError:'.format(err))
+		return -1
+	except:
+		print 'Unexpected error in test:', sys.exc_info()[0]
+		logging.debug('Unexpected error:')
+		raise
+	return cursor.lastrowid
+
+def doTask():
+    dbHandle = databaseHandler.dbConnectionCheck() # DB Open
+    cursor = dbHandle.cursor()
+    sqlStatement = "SELECT `app_pkg_name` FROM  `appdata`;"
+    try:
+        cursor.execute(sqlStatement)
+        for app in cursor:
+            app_pkg_name = app[0]
+            url = 'https://play.google.com/store/apps/details?id='+app_pkg_name
+            headers = { 'User-Agent' : 'Mozilla/5.0' }
+            req = urllib2.Request(url, None, headers)
+            try: 
+                page = urllib2.urlopen(req).read()
+                soup = bs(''.join(page))
+                app_dict = {}
+                app_dict['app_name'] = app_pkg_name
+                for div in soup.findAll(attrs={'class': 'score'}):
+                    for child in div.children:
+                        if not child.string == ' ':
+                            app_dict['review_rating'] = round(eval(child.string),1)
+                        else:
+                            app_dict['review_rating'] = 0.0
+                sqlStatement = "UPDATE `appdata` SET `review_rating`= "+app_dict['review_rating']+" WHERE `app_pkg_name` = "+app_pkg_name+";"
+                databaseHandler.dbManipulateData(dbHandle, sqlStatement)
+                # from pprint import pprint as pp
+                # pp(app_dict)
+            except urllib2.HTTPError, e:
+                print 'HTTPError =', str(e.code), 'for app:', app[0]
+                sqlStatement = "UPDATE `appdata` SET `still_in_googleplaystore`= 0 WHERE `app_pkg_name` = "+app_pkg_name+";"
+                databaseHandler.dbManipulateData(dbHandle, sqlStatement)
+    except:
+        print 'Unexpected error in updateReviewRatings:', sys.exc_info()[0]
+        raise
+
+    dbHandle.close() #DB Close
+
+def main(argv):
+    if len(sys.argv) != 1:
+        sys.stderr.write('Usage: python updateReviewRatings.py\n')
+        sys.exit(1)
+
+    startTime = time.time()
+    doTask()
+    executionTime = str((time.time()-startTime)*1000)
+    print "Execution time was: "+executionTime+" ms"
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))

adffa1c495f1e0bb0c8649ffe744eb303f60c392
Prajit Kumar Das <prajitdas@gmail.com>
2016-06-30 07:32:58 -0400

    paid or free

--- code/src/org/ebiquity/data/systemCallAnalysis/automateInstallationOfApp.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/automateInstallationOfApp.py
@@ -19,16 +19,38 @@ except:
 
 from com.dtmilano.android.viewclient import ViewClient
 
-vc = ViewClient(*ViewClient.connectToDeviceOrExit())
+package = 'edu.umbc.cs.ebiquity.autoinstallerapp'
+activity = 'edu.umbc.cs.ebiquity.autoinstallerapp.ui.activity.MainActivity'
+component = package + "/" + activity
 
-for bt in [ 'One', 'Two', 'Three', 'Four', 'Five' ]:
-    b = vc.findViewWithText(bt)
-    if b:
-        (x, y) = b.getXY()
-        print >>sys.stderr, "clicking b%s @ (%d,%d) ..." % (bt, x, y)
-        b.touch()
-    else:
-        print >>sys.stderr, "b%s not found" % bt
-    time.sleep(7)
+device, serialno = ViewClient.connectToDeviceOrExit(serialno='192.168.173.101:5555')
+device.startActivity(component=component)
+time.sleep(3)
+viewClient = ViewClient(device, serialno)
 
-print >>sys.stderr, "bye"
+appList = viewClient.findViewByIdOrRaise('edu.umbc.cs.ebiquity.autoinstallerapp:id/list')
+(x, y, w, h) = appList.getPositionAndSize()
+# start = (int(x+w/2.0), y+h)
+start = (int(x+w/2.0), y+h-250)
+end = (int(x+w/2.0), y-250)
+# end = (int(x+w/2.0), y)
+# scroll 5 times
+for i in range(5):
+    viewClient.device.drag(start, end, 1.0, 10)
+
+# installButton = viewClient.findViewWithText('INSTALL')
+# installButton.touch()
+#
+# acceptButton = viewClient.findViewWithTextOrRaise('ACCEPT')
+
+# for bt in [ 'INSTALL', 'ACCEPT' ]:
+#     b = vc.findViewWithText(bt)
+#     if b:
+#         (x, y) = b.getXY()
+#         print >>sys.stderr, "clicking b%s @ (%d,%d) ..." % (bt, x, y)
+#         b.touch()
+#     else:
+#         print >>sys.stderr, "b%s not found" % bt
+#     time.sleep(7)
+#
+# print >>sys.stderr, "bye"

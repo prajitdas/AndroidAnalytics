d3a711050cc47cc46ec196efe2f5a8076b09dce3
Prajit Kumar Das <prajitdas@gmail.com>
2016-07-02 00:08:41 -0400

    tfidf

---
+++ code/src/org/ebiquity/data/systemCallAnalysis/tfidf.py
@@ -0,0 +1,41 @@
+from sklearn.feature_extraction.text import TfidfTransformer
+import numpy as np
+import math
+import json
+
+def computeTFIDFWeights(termDocMatrix):
+	# sumFreqDict = {}
+	# for app in termDocMatrix:
+	# 	sumFreqDict[app] = float(sum(termDocMatrix[app][2]))
+	tfIDFDict = {}
+	for app in termDocMatrix:
+		tfVector = []
+		for syscallFreq in termDocMatrix[app][2]:
+			if syscallFreq > 0:
+				tfVector.append(math.log(1+syscallFreq, 2.71828))
+			else:
+				tfVector.append(0)
+		tfIDFDict[app] = tfVector
+
+	frequencyMatrix = []
+	for app in termDocMatrix:
+		frequencyMatrix.append(termDocMatrix[app][2])
+
+	syscallIDFVector = []
+	for column in zip(*frequencyMatrix):
+		syscallIDFVector.append(math.log((82.0/np.count_nonzero(np.array(column))), 2.71828))
+
+	for app in termDocMatrix:
+		termDocMatrix[app].pop()
+		tfIDFDict[app] = (np.ones(len(tfIDFDict[app])) * tfIDFDict[app] * syscallIDFVector).tolist()
+		termDocMatrix[app].append(tfIDFDict[app])
+
+	from pprint import pprint
+	pprint(termDocMatrix)
+
+	transformer = TfidfTransformer()
+	tfidf = transformer.fit_transform(frequencyMatrix)
+	print tfidf.toarray()[0]
+	print transformer.idf_
+
+	return termDocMatrix

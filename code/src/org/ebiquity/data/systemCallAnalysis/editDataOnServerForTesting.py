2d482911d7e3d87faa81b1608166545c4d0af5eb
Prajit Kumar Das <prajitdas@gmail.com>
2016-07-02 04:24:08 -0400

    fixing bugs

--- code/src/org/ebiquity/data/systemCallAnalysis/editDataOnServerForTesting.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/editDataOnServerForTesting.py
@@ -15,9 +15,6 @@ def getTimedBkpName():
 def storeDataonServer(jsonString):
 	# Server URL from Ngrok to get the data
 	url = 'https://9b5d5f5d.ngrok.io'
-	json.dump(jsonString, open('search.json', 'w'), sort_keys = True, indent = 4)
-	bkpJson = 'bkp'+getTimedBkpName()+'.json'
-	shutil.move('search.json', bkpJson)
 	serverResponse = requests.post(url, json.dumps(jsonString))
 	print serverResponse
 
@@ -25,4 +22,9 @@ def removeDataFromServer(appToRemove):
 	jsonString = json.loads(open('search.json','r').read())
 	if appToRemove in jsonString: 
 		del jsonString[appToRemove]
+
+	bkpJson = 'bkp'+getTimedBkpName()+'.json'
+	shutil.move('search.json', bkpJson)
+	json.dump(jsonString, open('search.json', 'w'), sort_keys = True, indent = 4)
+
 	storeDataonServer(jsonString)

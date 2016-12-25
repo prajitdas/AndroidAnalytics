faf377cbb83b0362a9a1e92d3c715a6111c743d7
Prajit Kumar Das <prajitdas@gmail.com>
2016-12-24 22:34:37 -0500

    adding permissions from Google into DB

--- code/src/org/ebiquity/data/collection/getDangerLevelOfPermisions.py
+++ code/src/org/ebiquity/data/collection/getDangerLevelOfPermisions.py
@@ -24,70 +24,71 @@ import json
 from collections import OrderedDict
 
 # Hit the Google GitHub URL, extract info about permissions and store in the
-def doTask():
-	dbHandle = databaseHandler.dbConnectionCheck()
+def readGooglePermissions(doInsertIntoDB):
 	url = "https://raw.githubusercontent.com/android/platform_frameworks_base/master/core/res/AndroidManifest.xml"
 	headers = {'User-Agent': 'Mozilla/5.0'}
 	req = urllib2.Request(url, None, headers)
+	outDict = {}
 	try:
 		file = urllib2.urlopen(req)
 		data = file.read()
 		dict = xmltodict.parse(data)
+		broadcasts = []
+		permGroups = []
+		permissions = []
 
-		with open('GoogleAndroidManifestPermissions.json', 'w') as fp:
+		with open('GoogleAndroidManifestPermissionsRaw.json', 'w') as fp:
 			json.dump(dict, fp, indent=4)
 
-		# for broadcasts in dict['manifest']['protected-broadcast']:
-			# od = OrderedDict(broadcasts)
-			# print od['@android:name']
-		# for permGroups in dict['manifest']['permission-group']:
-			# od = OrderedDict(permGroups)
-			# if '@android:name' in od:
-				# print od['@android:name']
-			# if '@android:label' in od:
-				# print od['@android:label']
-			# if '@android:description' in od:
-				# print od['@android:description']
-			# if '@android:permissionGroupFlags' in od:
-				# print od['@android:permissionGroupFlags']
-			# if '@android:priority' in od:
-				# print od['@android:priority']
-			# if '@android:icon' in od:
-				# print od['@android:icon']
-		for permissions in dict['manifest']['permission']:
-			od = OrderedDict(permissions)
-			name = ''
-			permissionGroup = ''
-			protectionLevel = ''
-			permissionFlags = ''
-			if '@android:name' in od:
-				name = od['@android:name']
-			if '@android:permissionGroup' in od:
-				permissionGroup = od['@android:permissionGroup']
-			if '@android:protectionLevel' in od:
-				protectionLevel = od['@android:protectionLevel']
-			if '@android:permissionFlags' in od:
-				permissionFlags = od['@android:permissionFlags']    
-			# if '@android:label' in od:
-				# print od['@android:label']
-			# if '@android:description' in od:
-				# print od['@android:description']
-			sqlStatement = "insert into permissions(name, protection_level, permission_group, permission_flags) values('"+name+"', '"+protectionLevel+"', '"+permissionGroup+"', '"+permissionFlags+"') on duplicate key update protection_level = '"+protectionLevel+"', permission_group = '"+permissionGroup+"', permission_flags = '"+permissionFlags+"';"
-			if permissionFlags == '':
-				sqlStatement = "insert into permissions(name, protection_level, permission_group, permission_flags) values('"+name+"', '"+protectionLevel+"', '"+permissionGroup+"', NULL) on duplicate key update protection_level = '"+protectionLevel+"', permission_group = '"+permissionGroup+"', permission_flags = NULL;"
+		# Added all Broadcasts from the file into the result set
+		for bcasts in dict['manifest']['protected-broadcast']:
+			od = OrderedDict(bcasts)
+			broadcast = {}
+			if ('@android:name' in od) and (not (od['@android:name'].startswith('@'))):
+				broadcast['name'] = od['@android:name']
+			broadcasts.append(broadcast)
+		outDict['broadcasts'] = broadcasts
 
-			databaseHandler.dbManipulateData(dbHandle, sqlStatement)
-		# soup = BeautifulSoup(page, 'xml')
-		# data = soup.findAll(attrs={'class': 'card-click-target'})
+		# Added all PermissionsGroups from the file into the result set
+		for pgrps in dict['manifest']['permission-group']:
+			od = OrderedDict(pgrps)
+			permGroup = {}
+			if ('@android:name' in od) and (not od['@android:name'].startswith('@')):
+				permGroup['name'] = od['@android:name']
+			if ('@android:priority' in od) and (not od['@android:priority'].startswith('@')):
+				permGroup['priority'] = od['@android:priority']
+			if ('@android:label' in od) and (not od['@android:label'].startswith('@')):
+				permGroup['label'] =  od['@android:label']
+			if ('@android:description' in od) and (not od['@android:description'].startswith('@')):
+				permGroup['description'] =  od['@android:description']
+			if ('@android:permissionGroupFlags' in od) and (not od['@android:permissionGroupFlags'].startswith('@')):
+				permGroup['permissionGroupFlags'] =  od['@android:permissionGroupFlags']
+			if ('@android:icon' in od) and (not od['@android:icon'].startswith('@')):
+				permGroup['icon'] =  od['@android:icon']
+			permGroups.append(permGroup)
+		outDict['permGroups'] = permGroups
 
-		# elementlist = xmldoc.getElementsByTagName('protected-broadcast')
+		# Added all Permissions from the file into the result set
+		for perms in dict['manifest']['permission']:
+			od = OrderedDict(perms)
+			permission = {}
+			if ('@android:name' in od) and (not od['@android:name'].startswith('@')):
+				permission['name'] = od['@android:name']
+			if ('@android:permissionGroup' in od) and (not od['@android:permissionGroup'].startswith('@')):
+				permission['permissionGroup'] = od['@android:permissionGroup']
+			if ('@android:protectionLevel' in od) and (not od['@android:protectionLevel'].startswith('@')):
+				permission['protectionLevel'] = od['@android:protectionLevel']
+			if ('@android:permissionFlags' in od) and (not od['@android:permissionFlags'].startswith('@')):
+				permission['permissionFlags'] = od['@android:permissionFlags']    
+			if ('@android:label' in od) and (not od['@android:label'].startswith('@')):
+				permission['label'] = od['@android:label']
+			if ('@android:description' in od) and (not od['@android:description'].startswith('@')):
+				permission['description'] = od['@android:description']
+			permissions.append(permission)
+			if doInsertIntoDB:
+				insertIntoDB(permission['name'],permission['protectionLevel'],permission['permissionGroup'],permission['permissionFlags'])
+		outDict['permissions'] = permissions
 
-		# for element in elementlist:
-		#	 print element
-			# packageName = url.split("=")
-
-			# sqlStatement = "SELECT * FROM permissions"
-			# databaseHandler.dbManipulateData(dbHandle, sqlStatement)
 	except urllib2.HTTPError, e:
 		print 'HTTPError = ', str(e.code)
 		#This is risky!!!!
@@ -102,15 +103,29 @@ def doTask():
 		print 'HTTPException'
 	except Exception:
 		print 'generic exception: ' + traceback.format_exc()
+	with open('GoogleAndroidManifestPermissions.json', 'w') as fp:
+		json.dump(outDict, fp, indent=4)
+
+def insertIntoDB(name, protectionLevel, permissionGroup, permissionFlags):
+	dbHandle = databaseHandler.dbConnectionCheck()
+	sqlStatement = "insert into permissions(name, protection_level, permission_group, permission_flags) values('"+name+"', '"+protectionLevel+"', '"+permissionGroup+"', '"+permissionFlags+"') on duplicate key update protection_level = '"+protectionLevel+"', permission_group = '"+permissionGroup+"', permission_flags = '"+permissionFlags+"';"
+	if permissionFlags == '':
+		sqlStatement = "insert into permissions(name, protection_level, permission_group, permission_flags) values('"+name+"', '"+protectionLevel+"', '"+permissionGroup+"', NULL) on duplicate key update protection_level = '"+protectionLevel+"', permission_group = '"+permissionGroup+"', permission_flags = NULL;"
+
+	databaseHandler.dbManipulateData(dbHandle, sqlStatement)
+
+def doTask(doInsertIntoDB):
+	readGooglePermissions(doInsertIntoDB)
 
 def main(argv):
-	if len(sys.argv) != 1:
-		sys.stderr.write('Usage: python getDangerLevelOfPermisions.py\n')
+	if len(sys.argv) != 2:
+		sys.stderr.write('Usage: python getDangerLevelOfPermisions.py doInsertIntoDB\n')
 		sys.exit(1)
 
 	startTime = time.time()
+	doInsertIntoDB = sys.argv[1]
 	#Start extracting data
-	doTask()
+	doTask(doInsertIntoDB)
 	executionTime = str((time.time()-startTime)*1000)
 	print "Execution time was: "+executionTime+" ms"
 

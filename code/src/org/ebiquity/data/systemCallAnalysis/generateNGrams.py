4b72b9605916b218586beac8233da2cc40e13a97
Prajit Kumar Das <prajitdas@gmail.com>
2017-03-10 12:44:29 -0500

    bug fix

--- code/src/org/ebiquity/data/systemCallAnalysis/generateNGrams.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/generateNGrams.py
@@ -17,9 +17,7 @@ logging.basicConfig(filename='generateNGrams.log',level=logging.DEBUG)
 def getData(appPkgName):
 	appPkgName = "com.onegogo.explorer"
 	appDataDict = json.loads(open(os.path.join(os.path.join(os.getcwd(),"uni-bi-tri-seq-jsons"),appPkgName+".json"),'r').read())
-	print type(appDataDict[appPkgName])
-	print appDataDict[appPkgName]
-	return appDataDict[appPkgName]["annotated_category"], appDataDict[appPkgName]["google_play_category"], appDataDict[appPkgName]
+	return appDataDict[appPkgName]["annotated_category"], appDataDict[appPkgName]["google_play_category"], appDataDict[appPkgName]["syscalls"]
 
 def find_ngrams(input_list, n):
 	return zip(*[input_list[i:] for i in range(n)])
@@ -37,13 +35,8 @@ def main(argv):
 		count += 1
 		print "Processing app number: "+str(count)+" named: "+appPkgName
 		annotated_category, google_play_category, input_list = getData(appPkgName)
-		print type(input_list)
-		# print find_ngrams(input_list,2)
-		sys.exit(1)
-	
-	input_list = ['all', 'this', 'happened', 'more', 'or', 'less']
-	print type(input_list)
-	print find_ngrams(input_list,3)
+		print find_ngrams(input_list,2)
+		print find_ngrams(input_list,3)
 	
 	executionTime = str((time.time()-startTime)*1000)
 	logging.debug('Execution time was: '+executionTime+' ms')

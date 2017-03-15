857d99f21fff756f4f43db40e38586737f6d5709
Prajit Kumar Das <prajitdas@gmail.com>
2017-03-15 19:40:01 -0400

    app perm info from a device

--- code/src/org/ebiquity/data/systemCallAnalysis/appInfo.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/appInfo.py
@@ -9,17 +9,17 @@ for line in p1.stdout.readlines():
 	all_perm_command="adb shell dumpsys package "+app+" | tr -d ' ' | sort | awk '/\.permission\./' | awk -F':' '{ print $1 }' | grep -v '{' | grep -v '\[' | uniq"
 	# adb shell dumpsys package com.facebook.katana | tr -d ' ' | sort | awk '/\.permission\./' | awk -F':' '{ print $1 }' | grep -v '{' | grep -v '\[' | uniq
 	p2=subprocess.Popen(all_perm_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	# app_perm={}
-	app_perm=[]
+	app_perm={}
+	# app_perm=[]
 	for newline in p2.stdout.readlines():
-		# app_perm[newline.strip()]=0
-		app_perm.append(newline.strip())
+		app_perm[newline.strip()]=0
+		# app_perm.append(newline.strip())
 
-	# perm_granted_command="adb shell dumpsys package "+app+" | tr -d ' ' | sort | awk '/granted=true/' | awk -F':' '{ print $1 }' | uniq"
-	# # adb shell dumpsys package com.facebook.katana | tr -d ' ' | sort | awk '/\.permission\./' | awk -F':' '{ print $1 }' | grep -v '{' | grep -v '\[' | uniq
-	# p3=subprocess.Popen(perm_granted_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-	# for newapp in p3.stdout.readlines():
-	# 	app_perm[newapp.strip()]=1
+	perm_granted_command="adb shell dumpsys package "+app+" | tr -d ' ' | sort | awk '/granted=true/' | awk -F':' '{ print $1 }' | uniq"
+	# adb shell dumpsys package com.facebook.katana | tr -d ' ' | sort | awk '/\.permission\./' | awk -F':' '{ print $1 }' | grep -v '{' | grep -v '\[' | uniq
+	p3=subprocess.Popen(perm_granted_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	for newapp in p3.stdout.readlines():
+		app_perm[newapp.strip()]=1
 	
 	perm_dict[app]=app_perm
 

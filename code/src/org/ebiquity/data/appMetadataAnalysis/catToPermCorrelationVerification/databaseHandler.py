ed39ec5b1ed52be52aa5c3aa902a8c81f72ac147
Prajit Kumar Das <prajitdas@users.noreply.github.com>
2017-05-05 19:56:45 -0400

    Update databaseHandler.py

--- code/src/org/ebiquity/data/appMetadataAnalysis/catToPermCorrelationVerification/databaseHandler.py
+++ code/src/org/ebiquity/data/appMetadataAnalysis/catToPermCorrelationVerification/databaseHandler.py
@@ -1,6 +1,9 @@
+# Note (Example is valid for Python v2 and v3)
+from __future__ import print_function
+
 '''
 Created on Apr 4, 2015
-Modified on June 22, 2016
+Modified on May 5, 2017
 @author: Prajit Kumar Das
 '''
 
@@ -9,6 +12,8 @@ import sys
 import time
 import logging
 import mysql.connector as mysql
+from mysql.connector import errorcode
+from mysql.connector.constants import ClientFlag
 logging.basicConfig(filename='databaseHandler.log',level=logging.DEBUG)
 
 # Fire an DML SQL statement and commit data
@@ -27,7 +32,7 @@ def dbManipulateData(dbHandle, sqlStatement):
 		logging.debug('ProgrammingError:'.format(err))
 		return -1
 	except:
-		print 'Unexpected error in test:', sys.exc_info()[0]
+		print('Unexpected error in test:'+str(sys.exc_info()[0]))
 		logging.debug('Unexpected error:')
 		raise
 	return cursor.lastrowid
@@ -37,24 +42,38 @@ def dbConnectionCheck():
 	parser = SafeConfigParser()
 	parser.read('dbconfig.ini')
 	
-	host = parser.get('dbconfig', 'host')
 	user = parser.get('dbconfig', 'user')
 	passwd = parser.get('dbconfig', 'passwd')
+	host = parser.get('dbconfig', 'host')
+	ssl_ca = parser.get('dbconfig', 'ssl_ca')
+	ssl_cert = parser.get('dbconfig', 'ssl_cert')
+	ssl_key = parser.get('dbconfig', 'ssl_key')
 	db = parser.get('dbconfig', 'db')
-	#print "info", user, passwd, host, db
+	#print("info"+user+passwd+host+ssl_ca+ssl_cert+ssl_key)
+
+	config = {
+		'user': user,
+		'password': passwd,
+		'host': host,
+		'client_flags': [ClientFlag.SSL],
+		'ssl_ca': ssl_ca,
+		'ssl_cert': ssl_cert,
+		'ssl_key': ssl_key,
+		'database': db,
+	}
+	
 	try:
-		dbHandle = mysql.connect(user=user, password=passwd, host=host, database=db)
-		#dbHandle.set_character_set('utf8')
+		dbHandle = mysql.connect(**config)
 		return dbHandle
 	except mysql.Error as err:
 		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-			print 'Something is wrong with your user name or password'
+			print('Something is wrong with your user name or password')
 			logging.debug('Something is wrong with your user name or password')
 		elif err.errno == errorcode.ER_BAD_DB_ERROR:
-			print 'Database does not exist'
+			print('Database does not exist')
 			logging.debug('Database does not exist')
 		else:
-			print err
+			print(err)
 			logging.debug('Something went wrong: {}'.format(err))
 	except Exception as err:
 		logging.debug('Something unexpected happened!'.format(err))
@@ -68,18 +87,21 @@ def convertPythonListToSQLQueryList(pythonList):
 
 def test():
 	dbHandle = dbConnectionCheck() #DB Open
-	cursor = dbHandle.cursor()
 	
-	sqlStatement = "SHOW TABLES;"
 	try:
+		cursor = dbHandle.cursor(buffered=True)
+		cursor.execute("SHOW STATUS LIKE 'Ssl_cipher'")
+		print(cursor.fetchone())
+		sqlStatement = "SHOW TABLES;"
 		cursor.execute(sqlStatement)
 		for Tables_in_googleplaystore in cursor:
-			print Tables_in_googleplaystore
+			print(Tables_in_googleplaystore)
 	except:
-		print 'Unexpected error in test:', sys.exc_info()[0]
+		print('Unexpected error in test:'+str(sys.exc_info()[0]))
 		logging.debug('Unexpected error:')
 		raise
 	
+	cursor.close()
 	dbHandle.close() #DB Close
 
 def main(argv):

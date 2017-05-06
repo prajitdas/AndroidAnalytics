7cbc626b658b5328669d1b2e437ff261fe4f5061
Prajit Kumar Das <prajitdas@gmail.com>
2017-05-05 21:15:44 -0400

    extract rules

--- code/src/org/ebiquity/data/xprivacy/extractRules.py
+++ code/src/org/ebiquity/data/xprivacy/extractRules.py
@@ -61,20 +61,30 @@ def insertIntoDB():
 	print sqlStatement
 	# sqlStatement="INSERT INTO `policy`(`id`, `app_id`, `accounts`, `browser`, `calendar`, `calling`, `clipboard`, `contacts`, `dict`, `email`, `identification`, `internet`, `ipc`, `location`, `media`, `messages`, `network`, `nfc`, `notifications`, `overlay`, `phone`, `sensors`, `shell`, `storage`, `system`, `webview`, `dt_modified`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10],[value-11],[value-12],[value-13],[value-14],[value-15],[value-16],[value-17],[value-18],[value-19],[value-20],[value-21],[value-22],[value-23],[value-24],[value-25],[value-26],[value-27])"
 
-def checkWhatsMissing():
-	policyDict=json.loads(open("compare.json",'r').read())
-	list1=policyDict["list1"]
-	list2=policyDict["list2"]
-	output=set(list1) - set(list2)
-	appDict={}
-	appDict["appNames"] = list(output)
-	open("applist.json",'w').write(json.dumps(appDict,sort_keys=True,indent=4))
+# def checkList():
+# 	policyDict=json.loads(open("applist.json",'r').read())
+# 	list=policyDict['appNames']
+# 	string="'"+'\',\''.join(list)+"'"
+# 	sqlStatement="SELECT a.app_pkg_name, a.review_count, a.review_rating, c.name FROM appdata a, appcategories c WHERE a.app_category_id = c.id AND a.app_pkg_name in ("+string+");"
+# 	print sqlStatement
+# 	print len(list)
+# 	# sqlStatement="INSERT INTO `policy`(`id`, `app_id`, `accounts`, `browser`, `calendar`, `calling`, `clipboard`, `contacts`, `dict`, `email`, `identification`, `internet`, `ipc`, `location`, `media`, `messages`, `network`, `nfc`, `notifications`, `overlay`, `phone`, `sensors`, `shell`, `storage`, `system`, `webview`, `dt_modified`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10],[value-11],[value-12],[value-13],[value-14],[value-15],[value-16],[value-17],[value-18],[value-19],[value-20],[value-21],[value-22],[value-23],[value-24],[value-25],[value-26],[value-27])"
+
+# def checkWhatsMissing():
+# 	policyDict=json.loads(open("compare.json",'r').read())
+# 	list1=policyDict["list1"]
+# 	list2=policyDict["list2"]
+# 	output=set(list1) - set(list2)
+# 	appDict={}
+# 	appDict["appNames"] = list(output)
+# 	open("applist.json",'w').write(json.dumps(appDict,sort_keys=True,indent=4))
 
 def main(argv):
 	startTime = time.time()
 	# generatePolicyJson()
-	#insertIntoDB()
-	checkWhatsMissing()
+	insertIntoDB()
+	# checkWhatsMissing()
+	# checkList()
 	executionTime = str((time.time()-startTime)/60)
 	print 'Execution time was: '+executionTime+' minutes'
 

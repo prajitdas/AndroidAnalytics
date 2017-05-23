1741e0242f6055bd5804b10b0aa7da749d02e4d2
Prajit Kumar Das <prajitdas@gmail.com>
2017-05-23 07:21:42 -0400

    data

--- code/src/org/ebiquity/data/systemCallAnalysis/runClassification.py
+++ code/src/org/ebiquity/data/systemCallAnalysis/runClassification.py
@@ -380,8 +380,8 @@ def doTFIDF(corpus, label, vectorizer):
 
 	sampleSize,featureSize=X_train.shape
 	size2k=2000 if featureSize > 2000 else featureSize
-	size4k=2000 if featureSize > 4000 else featureSize
-	size8k=2000 if featureSize > 8000 else featureSize
+	size4k=4000 if featureSize > 4000 else featureSize
+	size8k=8000 if featureSize > 8000 else featureSize
 	print X_train.shape, featureSize, sampleSize, size2k, size4k, size8k
 	return {}
 

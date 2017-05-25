5d8690ee0fd51bc0a52f247061b43aebfb41838e
Prajit Kumar Das <prajitdas@gmail.com>
2017-05-25 06:18:24 -0400

    data

---
+++ code/src/org/ebiquity/data/systemCallAnalysis/readResultsJSON.py
@@ -0,0 +1,49 @@
+import sys
+import time
+import json
+
+def processData():
+	resultsDict=json.loads(open("results.json","r").read())
+	dataDict=resultsDict["results"]["TFIDFResults"]
+	result={}
+	featureType="tfidf"
+	for data in dataDict:
+		if data.startswith("my"):
+			prefix="my"
+		else:
+			prefix="google"
+		grams=data.split("-")[1]
+		for ncomps in dataDict[data]:
+			for algo in dataDict[data][ncomps]:
+				for test in dataDict[data][ncomps][algo]:
+					if test.startswith("test") and not test.endswith("Report"):
+						result[prefix+","+featureType+","+grams+","+ncomps.split("c")[0]+","+algo+","+test.split("est")[1]]=dataDict[data][ncomps][algo][test]
+	dataDict=resultsDict["results"]["NGramResults"]
+	ncomps="all"
+	for data in dataDict:
+		if data.startswith("1"):
+			grams="uni"
+		elif data.startswith("2"):
+			grams="bi"
+		else:
+			grams="tri"
+		for prefix in dataDict[data]:
+			for featureType in dataDict[data][prefix]:
+				for algo in dataDict[data][prefix][featureType]:
+					for test in dataDict[data][prefix][featureType][algo]:
+						if test.startswith("test") and not test.endswith("Report"):
+							result[prefix+","+featureType+","+grams+","+ncomps+","+algo+","+test.split("est")[1]]=dataDict[data][prefix][featureType][algo][test]
+	open("processedResults.json","w").write(json.dumps(result, sort_keys=True, indent=4))
+
+def main(argv):
+	if len(sys.argv) != 1:
+		sys.stderr.write('Usage: python readResultsJSON.py')
+		sys.exit(1)
+
+	startTime = time.time()
+	processData()
+	executionTime = str((time.time()-startTime)*1000)
+	print 'Execution time was: '+executionTime+' ms'
+
+if __name__ == "__main__":
+	main(sys.argv)

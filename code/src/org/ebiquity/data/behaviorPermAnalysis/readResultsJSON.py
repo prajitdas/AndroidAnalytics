2173ac1ac698f42fb6e2cdde2bc25d91f78279e9
Prajit Kumar Das <prajitdas@gmail.com>
2017-08-11 19:46:15 -0400

    app changes

---
+++ code/src/org/ebiquity/data/behaviorPermAnalysis/readResultsJSON.py
@@ -0,0 +1,47 @@
+import sys
+import time
+import json
+
+classifiers = ["Nearest Neighbors",
+		 "Linear SVM",
+		 "RBF SVM",
+		 "Decision Tree",
+		 "Random Forest",
+		 "Neural Net",
+		 "AdaBoost",
+		 "Naive Bayes",
+		 "Logistic Regression",
+		 "Dummy"]
+
+def writecsv():
+	resultsDict=json.loads(open("processedResults.json","r").read())
+	with open("results.csv", "wb") as file:
+		for key in resultsDict:
+			line=key+","+str(resultsDict[key])
+			file.write(line)
+			file.write('\n')
+
+def processData():
+	resultsDict=json.loads(open("results.json","r").read())
+	result={}
+	for classifier in classifiers:
+		print "Processing results for: ", classifier
+		dataDict=resultsDict[classifier]
+		for data in dataDict:
+			if data.startswith("test") and not data.endswith("Report"):
+				result[classifier+","+data.split("test")[1]]=dataDict[data]
+	open("processedResults.json","w").write(json.dumps(result, sort_keys=True, indent=4))
+
+def main(argv):
+	if len(sys.argv) != 1:
+		sys.stderr.write('Usage: python readResultsJSON.py')
+		sys.exit(1)
+
+	startTime = time.time()
+	processData()
+	writecsv()
+	executionTime = str((time.time()-startTime)*1000)
+	print 'Execution time was: '+executionTime+' ms'
+
+if __name__ == "__main__":
+	main(sys.argv)
